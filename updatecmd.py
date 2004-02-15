2733f400fafc355b3fa98d7e82a2d746badf388e
ewt <devnull@localhost>
2004-02-15 02:01:32 +0000

    don't chown or chgrp symlinks

--- updatecmd.py
+++ updatecmd.py
@@ -5,6 +5,7 @@ import util
 import shutil
 import pwd
 import grp
+import files
 
 def doUpdate(DBPATH, root, pkgName):
     pkgSet = package.PackageSet(DBPATH, pkgName)
@@ -18,19 +19,22 @@ def doUpdate(DBPATH, root, pkgName):
 	infoFile = files.FileDB(DBPATH, fileName)
 	f = infoFile.getVersion(version)
 
+	source = "%s/files/%s.contents/%s" % (DBPATH, fileName, f.uniqueName())
+
 	target = "%s/%s" % (root, fileName)
 	dir = os.path.split(target)[0]
 	util.mkdirChain(dir)
 
-	source = "%s/files/%s.contents/%s" % (DBPATH, fileName, f.uniqueName())
-
 	f.copy(source, target)
-	os.chmod(target, f.perms())
 
-	if not os.getuid():
-	    # root should set the file ownerships properly
-	    uid = pwd.getpwnam(f.owner())[2]
-	    gid = grp.getgrnam(f.group())[2]
+	if (f.__class__ != files.SymbolicLink):
+	    os.chmod(target, f.perms())
 
-	    # FIXME: this needs to use lchown, which is in 2.3
-	    os.chown(target, uid, gid)
+	    if not os.getuid():
+		# root should set the file ownerships properly
+		uid = pwd.getpwnam(f.owner())[2]
+		gid = grp.getgrnam(f.group())[2]
+
+		# FIXME: this needs to use lchown, which is in 2.3, and
+		# this should happen unconditionally
+		os.chown(target, uid, gid)
