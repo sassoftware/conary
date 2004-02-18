5708a6560c524dc17bd460cbe48d478928608eec
ewt <devnull@localhost>
2004-02-18 20:42:11 +0000

    commit operation is toast

--- commit.py
+++ commit.py
@@ -40,26 +40,3 @@ def finalCommit(reppath, pkgName, version, root, fileList):
 	file.archive(reppath, root)
 
     pkgSet.write()
-
-def doCommit(reppath, pkgName, version, root, fileNameFile):
-    f = open(fileNameFile, "r")
-    list = []
-    for n in f.readlines():
-	list.append(n[:-1])	# chop
-    f.close()
-
-    fileList = []
-    for i in range(0, len(list)):
-	frags = string.split(list[i])
-	path = frags[0]
-	flags = frags[1:]
-
-	type = "auto"
-	for flag in flags:
-	    if flag == "src":
-		type = "src"
-
-	f = files.FileFromFilesystem(root, path, type)
-	fileList.append(f)
-
-    finalCommit(reppath, pkgName, version, root, fileList)
