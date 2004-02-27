3e18cd1386b5566354db7318464e4b60718eb80c
ewt <devnull@localhost>
2004-02-27 08:40:24 +0000

    tried to change the api to use cfg as a passed object; may have gotten it wrong

--- build/lookaside.py
+++ build/lookaside.py
@@ -8,20 +8,17 @@ import os
 import util
 import urllib2
 import time
-import srscfg
-
-cfg = srscfg.SrsConfiguration()
 
 # location is normally the package name
 
-def createCacheName(name, location, negative=''):
+def createCacheName(cfg, name, location, negative=''):
     cachedname = '%s/%s%s/%s' %(cfg.lookaside, negative, location, name)
 
-def createCacheEntry(name, location, infile):
+def createCacheEntry(cfg, name, location, infile):
     # cache needs to be hierarchical to avoid collisions, thus we
     # use location so that files with the same name and different
     # contents in different packages do not collide
-    cachedname = createCacheName(name, location)
+    cachedname = createCacheName(cfg, name, location)
     f = open(cachedname, "w+")
     f.write(infile.read())
     f.close()
@@ -29,12 +26,12 @@ def createCacheEntry(name, location, infile):
     return cachedname
 
 
-def createNegativeCacheEntry(name, location):
+def createNegativeCacheEntry(cfg, name, location):
     negativeEntry = createCacheName(name, location, 'NEGATIVE/')
     open(negativeEntry, "w+").close()
 
 
-def searchCache(name, location):
+def searchCache(cfg, name, location):
     basename = os.path.basename(name)
 
     if name.startswith("http://") or name.startswith("ftp://"):
@@ -50,44 +47,44 @@ def searchCache(name, location):
     return util.searchFile(basename, '%s/%s' %(cfg.lookaside, location))
 
 
-def searchRepository(name, location):
+def searchRepository(cfg, name, location):
     """searches repository, and retrieves to cache"""
     basename = os.path.basename(name)
     # FIXME: I don't know how to do this yet
     return None
 
 
-def searchAll(name, location, srcdirs):
+def searchAll(cfg, name, location, srcdirs):
     """searches all locations, including populating the cache if the
     file can't be found in srcdirs, and returns the name of the file"""
     f = util.searchFile(name, srcdirs)
     if f: return f
 
-    f = searchCache(name, location)
+    f = searchCache(cfg, name, location)
     if f: return f
 
-    f = searchRepository(name, location)
+    f = searchRepository(cfg, name, location)
     if f: return f
 
     if name.startswith("http://") or name.startswith("ftp://"):
 	try:
 	    url = urllib2.urlopen(name)
 	except urllib2.URLError:
-	    createNegativeCacheEntry(name[5:], location)
+	    createNegativeCacheEntry(cfg, name[5:], location)
 	    return None
-	return createCacheEntry(name[5:], location, url)
+	return createCacheEntry(cfg, name[5:], location, url)
 
     return None
 
 
-def findAll(name, location, srcdirs):
-    f = searchAll(name, location, srcdirs)
+def findAll(cfg, name, location, srcdirs):
+    f = searchAll(cfg, name, location, srcdirs)
     if not f:
 	raise OSError, (errno.ENOENT, os.strerror(errno.ENOENT))
     return f
 
 
-class LookAside(file):
+class LookAside(cfg, file):
     def __init__(name, location, srcdirs, buffered=-1):
 	f = findAll(name, location, srcdirs)
 	file.__init__(self, f, "r+", buffered)
