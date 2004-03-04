1894dd77ec40fbe2c21a6fbdfd6464fa566e43ec
ewt <devnull@localhost>
2004-03-04 20:34:21 +0000

    1) killed finalCommit() 2) added ability to commit (currently only abstract) changesets

--- commit.py
+++ commit.py
@@ -2,40 +2,80 @@
 # Copyright (c) 2004 Specifix, Inc.
 # All rights reserved
 #
-import package
+import changeset
 import copy
+import files
+import package
+import sys
 
-# version is a short package version, not an SRS version string
-def finalCommit(repos, cfg, pkgName, simpleVersion, fileList):
-    pkgSet = repos.getPackageSet(pkgName, "w")
+def commitChangeSet(repos, cfg, changeSetFile):
+    cs = changeset.ChangeSet()
+    cs.useFile(changeSetFile)
 
-    version = pkgSet.getLatestVersion(cfg.defaultbranch)
-    if version and simpleVersion == version.trailingVersion():
-	# yes, increment the revision
-	version = copy.deepcopy(version)
-	version.incrementVersionRelease()
-    else:
-	# no, make a new version
-	version = copy.deepcopy(cfg.defaultbranch)
-	version.appendVersionRelease(simpleVersion, 1)
+    pkgList = []
+    fileMap = {}
 
-    p = package.Package(version)
+    # build todo set
+    for pkg in cs.getPackageList():
+	if pkg.getOldVersion():
+	    raise IOError, "only abstract change sets are currently supported "
 
-    for (file, pathToFile, pathInPkg) in fileList:
-	infoFile = repos.getFileDB(file.id())
+	newVersion = pkg.getNewVersion()
+    
+	if repos.hasPackage(pkg.getName()):
+	    pkgSet = repos.getPackageSet(pkg.getName(), "r")
 
-	duplicateVersion = infoFile.checkBranchForDuplicate(cfg.defaultbranch,
-					file)
-	if not duplicateVersion:
-	    infoFile.addVersion(version, file)
-
-	    p.addFile(file.id(), pathInPkg, version)
-
-	    infoFile.close()
+	    if pkgSet.hasVersion(newVersion):
+		raise KeyError, "version %s for %s exists" % \
+			(newVersion.asString(), pkg.getName())
 	else:
-	    p.addFile(file.id(), pathInPkg, duplicateVersion)
+	    pkgSet = None
 
-	file.archive(repos, pathToFile)
+	newPkg = package.Package(newVersion)
 
-    pkgSet.addVersion(version, p)
-    pkgSet.close()
+	for (fileId, path, fileVersion) in pkg.getNewFileList():
+	    info = cs.getFileChange(fileId)
+	    file = files.FileFromInfoLine(info, fileId)
+
+	    newPkg.addFile(fileId, path, fileVersion)
+
+	    infoFile = repos.getFileDB(fileId)
+	    if not infoFile.hasVersion(fileVersion):
+		# abstract packages often contain file
+		fileMap[fileId] = (fileVersion, file, path)
+	
+	pkgList.append((pkg.getName(), newPkg, newVersion))
+
+    # commit changes
+    pkgsDone = []
+    filesDone = []
+    try:
+	for (pkgName, newPkg, newVersion) in pkgList:
+	    pkgSet = repos.getPackageSet(pkgName, "w")
+	    pkgSet.addVersion(newVersion, newPkg)
+	    pkgsDone.append((pkgSet, newVersion))
+
+	for (fileId, (fileVersion, file, path)) in fileMap.items():
+	    infoFile = repos.getFileDB(fileId)
+	    infoFile.addVersion(fileVersion, file)
+	    infoFile.close()
+	    filesDone.append(fileId)
+
+	for (fileId, (fileVersion, file, path)) in fileMap.items():
+	    if isinstance(file, files.RegularFile):
+		f = cs.getFileContents(file.sha1())
+
+		file.archive(repos, f)
+
+		f.close()
+    except:
+	# something went wrong; try to unwind our commits
+	for fileId in filesDone:
+	    infoFile = repos.getFileDB(fileId)
+	    (fileVersion, file, path) = fileMap[fileId]
+	    infoFile.eraseVersion(fileVersion)
+
+	for (pkgSet, newVersion) in pkgsDone:
+	    pkgSet.eraseVersion(newVersion)
+
+	raise 
