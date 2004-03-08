ba1c0bebc663738f891e3f5260fbc3d618d10974
ewt <devnull@localhost>
2004-03-08 20:34:48 +0000

    supports differential change sets

--- commit.py
+++ commit.py
@@ -17,10 +17,8 @@ def commitChangeSet(repos, cfg, changeSetFile):
 
     # build todo set
     for pkg in cs.getPackageList():
-	if pkg.getOldVersion():
-	    raise IOError, "only abstract change sets are currently supported "
-
 	newVersion = pkg.getNewVersion()
+	old = pkg.getOldVersion()
     
 	if repos.hasPackage(pkg.getName()):
 	    pkgSet = repos.getPackageSet(pkg.getName(), "r")
@@ -31,20 +29,28 @@ def commitChangeSet(repos, cfg, changeSetFile):
 	else:
 	    pkgSet = None
 
-	newPkg = package.Package(newVersion)
+	if old:
+	    newPkg = copy.deepcopy(pkgSet.getVersion(old))
+	else:
+	    newPkg = package.Package(newVersion)
 
-	for (fileId, path, fileVersion) in pkg.getNewFileList():
-	    info = cs.getFileChange(fileId)
-	    file = files.FileFromInfoLine(info, fileId)
-
-	    newPkg.addFile(fileId, path, fileVersion)
-
-	    infoFile = repos.getFileDB(fileId)
-	    if not infoFile.hasVersion(fileVersion):
-		# abstract packages often contain file
-		fileMap[fileId] = (fileVersion, file, path)
-	
+	newFileMap = newPkg.applyChangeSet(repos, pkg)
 	pkgList.append((pkg.getName(), newPkg, newVersion))
+	fileMap.update(newFileMap)
+
+    # create the file objects we'll need for the commit
+    fileList = []
+    for (fileId, (oldVer, newVer, infoLine)) in cs.getFileList():
+	if oldVer:
+	    fileDB = repos.getFileDB(fileId)
+	    file = copy.deepcopy(fileDB.getVersion(oldVer))
+	    file.applyChange(infoLine)
+	    del fileDB
+	else:
+	    file = files.FileFromInfoLine(infoLine, fileId)
+
+	assert(newVer.equal(fileMap[fileId][1]))
+	fileList.append((fileId, newVer, file))
 
     # commit changes
     pkgsDone = []
@@ -55,24 +61,22 @@ def commitChangeSet(repos, cfg, changeSetFile):
 	    pkgSet.addVersion(newVersion, newPkg)
 	    pkgsDone.append((pkgSet, newVersion))
 
-	for (fileId, (fileVersion, file, path)) in fileMap.items():
+	for (fileId, fileVersion, file) in fileList:
 	    infoFile = repos.getFileDB(fileId)
 	    infoFile.addVersion(fileVersion, file)
 	    infoFile.close()
 	    filesDone.append(fileId)
 
-	for (fileId, (fileVersion, file, path)) in fileMap.items():
+	for (fileId, fileVersion, file) in fileList:
 	    if isinstance(file, files.RegularFile):
 		f = cs.getFileContents(file.sha1())
-
 		file.archive(repos, f)
-
 		f.close()
     except:
 	# something went wrong; try to unwind our commits
 	for fileId in filesDone:
 	    infoFile = repos.getFileDB(fileId)
-	    (fileVersion, file, path) = fileMap[fileId]
+	    (fileVersion, path) = fileMap[fileId]
 	    infoFile.eraseVersion(fileVersion)
 
 	for (pkgSet, newVersion) in pkgsDone:
