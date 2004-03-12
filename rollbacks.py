ef8f81af733cdd16af6556c9cadaf5cc4ca1f3cb
ewt <devnull@localhost>
2004-03-12 02:30:47 +0000

    added "rollbacks" command

---
+++ rollbacks.py
@@ -0,0 +1,17 @@
+import os
+import package
+
+def listRollbacks(db, cfg):
+    for rollbackName in db.getRollbackList():
+	print "%s:" % os.path.basename(rollbackName)
+
+	rb = db.getRollback(rollbackName)
+
+	for pkg in rb.getPackageList():
+	    print "\t%s %s -> %s" % \
+		(package.stripNamespace(cfg.packagenamespace, pkg.getName()),
+		 pkg.getOldVersion().asString(cfg.defaultbranch), 
+		 pkg.getNewVersion().asString(cfg.defaultbranch))
+
+	print
+
