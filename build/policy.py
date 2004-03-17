8bc735c30a9497cc715c86f5c40ef003d0a8971d
johnsonm <devnull@localhost>
2004-03-17 23:52:28 +0000

    invariants, gzip and info policies

--- build/policy.py
+++ build/policy.py
@@ -22,7 +22,32 @@ class Policy(util.Action):
     doFile member.  The doFile function is never called for files
     that match an exception regexp; do functions must implement
     their own exception regexp handling.
+
+    The following variables apply automatically to the doFile member;
+    classes implementing the do member instead may use them as well,
+    but should use the same rules if they do use them.  All of them
+    have self.macros applied before use.
+
+    @var invariantsubtrees: if invariantsubtrees is not empty,
+    then it is a list of subtrees (relative to %(destdir)s) to
+    walk INSTEAD of walking the entire %(destdir)s tree.
+
+    @var invariantinclusions: if invariantinclusions is not empty,
+    then only files matching a regular expression in it are
+    considered to be passed to to the doFile member.  Any exclusions
+    including invariants, are applied after invariantinclusions
+    are applied.
+
+    @var invariantexceptions: subclasses may set to a list of
+    exception regular expressions that are always applied regardless
+    of what other exceptions may be provided by the recipe; these
+    exceptions being applied is an invariant condition of the doFile
+    member.
     """
+    invariantsubtree = []
+    invariantexceptions = []
+    invariantinclusions = []
+
     def __init__(self, *args, **keywords):
 	"""
 	@param exceptions: Optional argument; regexp(s) specifying
@@ -58,8 +83,15 @@ class Policy(util.Action):
 	# change self.use to be a simple flag
 	self.use = util.checkUse(self.use)
 
+	# compile the inclusions
+	self.inclusionREs = []
+	for inclusion in self.invariantinclusions:
+	    self.inclusionREs.append(re.compile(inclusion %self.macros))
+
 	# compile the exceptions
 	self.exceptionREs = []
+	for exception in self.invariantexceptions:
+	    self.exceptionREs.append(re.compile(exception %self.macros))
 	if self.exceptions:
 	    if not isinstance(self.exceptions, (tuple, list)):
 		self.exceptions = (self.exceptions,)
@@ -68,10 +100,14 @@ class Policy(util.Action):
 
 	# dispatch if/as appropriate
 	if self.use:
-	    if self.__class__.__dict__.has_key('do'):
+	    if hasattr(self.__class__, 'do'):
 		self.do()
-	    elif self.__class__.__dict__.has_key('doFile'):
-		os.path.walk(self.macros['destdir'], _walkFile, self)
+	    elif hasattr(self.__class__, 'doFile'):
+		if not self.invariantsubtree:
+		    self.invariantsubtree.append('/')
+		for subtree in self.invariantsubtree:
+		    os.path.walk('%(destdir)s/'+subtree %self.macros,
+				 _walkFile, self)
 
 
 # internal helpers
@@ -81,14 +117,23 @@ def _policyException(policyObj, filespec):
 	if re.search(filespec):
 	    return True
     return False
-    
+
+def _policyInclusion(policyObj, filespec):
+    if not policyObj.inclusionREs:
+	# empty list is '.*'
+	return True
+    for re in policyObj.inclusionREs:
+	if re.search(filespec):
+	    return True
+    return False
 
 def _walkFile(policyObj, dirname, names):
     # chop off bit not useful for comparison
     path=dirname[len(policyObj.macros['destdir']):]
     for name in names:
 	thispath = path + os.sep + name
-	if not _policyException(policyObj, thispath):
+	if _policyInclusion (policyObj, thispath) and \
+	   not _policyException(policyObj, thispath):
 	    policyObj.doFile(thispath)
 
 
@@ -105,12 +150,10 @@ class RemoveExtraLibs(Policy):
     """
     Kill .la files and any other similar garbage
     """
-    def __init__(self, *args, **keywords):
-	Policy.__init__(self, *args, **keywords)
-	self.re = re.compile('\.la')
+    invariantinclusion = ['\.la$']
+
     def doFile(self, path):
-	if self.re.search(path):
-	    util.remove(self.macros['destdir']+path)
+	util.remove(self.macros['destdir']+path)
 
 class Strip(Policy):
     """
@@ -126,13 +169,30 @@ class StripToDebug(Policy):
     def do(self):
 	pass
 
+class NormalizeGzip(Policy):
+    """
+    re-gzip .gz files with -9 -n
+    Ignore man/info pages, we'll get them separately while fixing
+    other things
+    """
+    invariantexceptions = [
+	'%(mandir)s/man.*/.*',
+	'%(infodir)s/.*',
+    ]
+    invariantinclusions = [
+	'.*\.gz'
+    ]
+    def doFile(self, path):
+	util.execute('gunzip %s' %path);
+	util.execute('gzip -n -9 %s' %path[:-3])
+
 class NormalizeManPages(Policy):
     def do(self):
 	pass
 
 class NormalizeInfoPages(Policy):
     """
-    compress info files and remove dir file (unless is exception)
+    compress info files and remove dir file
     """
     def do(self):
 	dir = self.macros['infodir']+'/dir'
@@ -140,15 +200,16 @@ class NormalizeInfoPages(Policy):
 	if os.path.exists(fsdir):
 	    if not _policyException(self, dir):
 		util.remove(fsdir)
-	# XXX finish the job!
-
-class RemoveTimeStamps(Policy):
-    """
-    Remove time/date stamps from compressed files and archives
-    XXX msw will have to implement this, I have no idea how
-    """
-    def do(self):
-	pass
+	if os.path.isdir('%(destdir)s/%(infodir)s' %self.macros):
+	    infofiles = os.listdir('%(destdir)s/%(infodir)s' %self.macros)
+	    for file in infofiles:
+		syspath = '%(destdir)s/%(infodir)s/' %self.macros + file
+		path = '%(infodir)s/' %self.macros + file
+		if not _policyException(self, path):
+		    if file.endswith('.gz'):
+			util.execute('gunzip %s' %syspath)
+			syspath = syspath[:-3]
+		    util.execute('gzip -n -9 %s' %syspath)
 
 
 def DefaultPolicy():
@@ -160,7 +221,7 @@ def DefaultPolicy():
 	SanitizeSonames(),
 	RemoveExtraLibs(),
 	StripToDebug(),
+	NormalizeGzip(),
 	NormalizeManPages(),
 	NormalizeInfoPages(),
-	RemoveTimeStamps(),
     ]
