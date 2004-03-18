49673769e541c1537464764640251c519065acf1
msw <devnull@localhost>
2004-03-18 23:25:17 +0000

    modify doc grouping

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 The Specifix Repository System
 
-@group Package Building: build, buildpackage, recipe, cook
-@group Utility: util, lookaside, rpmhelper, sha1helper
+@group Package Building: build, buildpackage, recipe, cook, policy, destdirpolicy
+@group Utility: util, lookaside, rpmhelper, sha1helper, fixedglob
 @group Files: datastore, files, filecontainer
 """
