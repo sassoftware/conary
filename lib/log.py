c7a9f5fad7f9b636110ecd177f28b8773725a418
ewt <devnull@localhost>
2004-03-24 02:49:10 +0000

    arguments should get formatted

--- lib/log.py
+++ lib/log.py
@@ -1,11 +1,11 @@
 import logging
 import sys
 
-def error(message):
-    logger.log(logging.ERROR, message)
+def error(message, *args):
+    logger.log(logging.ERROR, message % args)
 
-def warning(message):
-    logger.warning(message)
+def warning(message, *args):
+    logger.warning(message % args)
 
 if not globals().has_key("logger"):
     logging.addLevelName(logging.WARNING, "warning")
