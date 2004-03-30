85e4916a7e0a19d79cc032efff37172b6314799c
msw <devnull@localhost>
2004-03-30 01:05:59 +0000

    add copyright and docstrings

--- lib/log.py
+++ lib/log.py
@@ -1,16 +1,29 @@
+#
+# Copyright (c) 2004 Specifix, Inc.
+# All rights reserved
+#
+
+"""
+Implements the logging facility for srs
+"""
+
 import logging
 import sys
 
 def error(*args):
+    "Log an error"
     logger.error(*args)
 
 def warning(*args):
+    "Log a warning"    
     logger.warning(*args)
 
 def info(*args):
+    "Log an informative message"
     logger.info(*args)
 
 def debug(*args):
+    "Log a debugging message"
     logger.debug(*args)
 
 if not globals().has_key("logger"):
