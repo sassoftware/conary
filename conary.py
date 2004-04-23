46704ddb9b0711bdac3614a98279a100a3adb222
ewt <devnull@localhost>
2004-04-23 19:29:55 +0000

    switched to running srs.py instead of srs; this makes the contents of srs importable (and testable)

---
+++ conary.py
@@ -0,0 +1,305 @@
+#!/usr/bin/python2.3
+# -*- mode: python -*-
+#
+# Copyright (c) 2004 Specifix, Inc.
+# All rights reserved
+#
+
+import branch
+import changeset
+import commit
+import cook
+import database
+import display
+import log
+import os
+import repository
+import rollbacks
+import srscfg
+import srcctl
+import sys
+import updatecmd
+import util
+
+if sys.version_info < (2, 3):
+    print "error: python 2.3 or greater is requried"
+    sys.exit(1)
+
+sys.excepthook = util.excepthook
+
+try:
+    cfg = srscfg.SrsConfiguration()
+except srscfg.ParseError, e:
+    log.error(str(e))
+    sys.exit(1)
+
+def usage(rc = 1):
+    print "usage: srs branch <newbranch> <branchfrom> [<trove>]"
+    print "       srs changeset <pkg> [<oldver>] <newver> <outfile>"
+    print "       srs cook    [--prep] [--macros file] <first.recipe> <second.recipe> ..."
+    print "       srs commit       <changeset>"
+    print "       srs erase        <pkgname> [<version>]"
+    print "       srs localcs      <pkg> <outfile>"
+    print "       srs localcommit  <changeset>"
+    print "       srs pkglist      <pkgname> [<version>]"
+    print "       srs replist      <pkgname> [<version>]"
+    print "       srs rblist"
+    print "       srs rollback     <rollback>"
+    print "       srs source       [usage]"
+    print "       srs update	   <pkgname> <version>"
+    print "           update       <changeset>"
+    print "       srs usage"
+    print ""
+    print "commit flags:  --target-branch <branch>"
+    print ""
+    print "common flags:  --branch <defaultbranch>"
+    print '               --config "<item> <value>"'
+    print "               --reppath <repository-path>"
+    print "               --root <root>"
+    print ""
+    print "pkglist flags: --all"
+    print "               --ls"
+    print ""
+    print "update flags: --replace-files"
+    return rc
+
+def openRepository(path, mode):
+    try:
+        repos = repository.FilesystemRepository(path, mode)
+    except repository.OpenError, e:
+        log.error('Unable to open repository %s: %s', path, str(e))
+        sys.exit(1)
+    return repos
+
+def openDatabase(root, path, mode):
+    try:
+        db = database.Database(root, path, mode)
+    except repository.OpenError, e:
+        log.error('Unable to open database %s%s%s: %s', root, os.sep, path, str(e))
+        sys.exit(1)
+    return db
+
+def main():
+    otherArgs = [ sys.argv[0] ]
+    argSet = {}
+    argDef = {}
+    cfgMap = {}
+    # 0 - arg may occur, no parameter
+    # 1 - arg may occur once, w/ parameter
+    # 2 - arg may occur N times, w/ parameter
+
+    cfgMap["branch"] = "defaultbranch"
+    cfgMap["reppath"] = "reppath"
+    cfgMap["root"] = "root"
+
+    argDef["all"] = 0
+    argDef["config"] = 2
+    argDef["ls"] = 0
+    argDef["macros"] = 1
+    argDef["prep"] = 0
+    argDef["profile"] = 0
+    argDef["replace-files"] = 0
+    argDef["target-branch"] = 1
+
+    argDef.update(srcctl.argDef)
+
+    for arg in cfgMap.keys():
+	argDef[arg] = 1
+
+    i = 1
+    while i < len(sys.argv):
+	if sys.argv[i][:2] != "--":
+	    otherArgs.append(sys.argv[i])
+	else:
+	    arg = sys.argv[i][2:]
+	    if not argDef.has_key(arg): return usage()
+
+	    if not argDef[arg]:
+		argSet[arg] = 1
+	    else:
+		# the argument takes a parameter
+		i = i + 1
+		if i >= len(sys.argv): return usage()
+
+		if argDef[arg] == 1:
+		    # exactly one parameter is allowd
+		    if argSet.has_key(arg): return usage()
+		    argSet[arg] = sys.argv[i]
+		else:
+		    # multiple parameters may occur
+		    if argSet.has_key(arg):
+			argSet[arg].append(sys.argv[i])
+		    else:
+			argSet[arg] = [sys.argv[i]]
+
+	i = i + 1
+
+    if '-v' in otherArgs:
+	otherArgs.remove('-v')
+	log.setVerbosity(1)
+    else:
+	log.setVerbosity(0)
+
+    profile = False
+    if argSet.has_key('profile'):
+	import hotshot
+	prof = hotshot.Profile('srs.prof')
+	prof.start()
+	profile = True
+	del argSet['profile']
+
+    if argSet.has_key('config'):
+	for param in argSet['config']:
+	    cfg.configLine(param)
+
+	del argSet['config']
+
+    for (arg, name) in cfgMap.items():
+	if argSet.has_key(arg):
+	    cfg.configLine("%s %s" % (name, argSet[arg]))
+	    del argSet[arg]
+
+    if (len(otherArgs) < 2):
+	return usage()
+    elif (otherArgs[1] == "branch"):
+	if argSet: return usage
+	if len(otherArgs) < 4 or len(otherArgs) > 5: return usage()
+	repos = openRepository(cfg.reppath, "c")
+
+	args = [repos, cfg.packagenamespace ] + otherArgs[2:]
+	branch.branch(*args)
+    elif (otherArgs[1] == "changeset"):
+	# current usage is "package file oldversion newversion"
+	if len(otherArgs) != 5 and len(otherArgs) != 6:
+	    return usage()
+
+	name = otherArgs[2]
+	if len(otherArgs) == 6:
+	    (old, new) = (otherArgs[3], otherArgs[4])
+	    outFile = otherArgs[5]
+	else:
+	    (old, new) = (None, otherArgs[3])
+	    outFile = otherArgs[4]
+
+	repos = openRepository(cfg.reppath, "r")
+
+	changeset.ChangeSetCommand(repos, cfg, name, outFile, old, new)
+    elif (otherArgs[1] == "commit"):
+	targetBranch = None
+	if argSet.has_key('target-branch'):
+	    targetBranch  = argSet['target-branch']
+	    del argSet['target-branch']
+	if len(otherArgs) < 3: return usage()
+	repos = openRepository(cfg.reppath, "c")
+	for changeSet in otherArgs[2:]:
+	    commit.doCommit(repos, changeSet, targetBranch)
+    elif (otherArgs[1] == "config"):
+	if argSet: return usage
+	if (len(otherArgs) > 2):
+	    return usage()
+	else:
+	    cfg.display()
+    elif (otherArgs[1] == "cook"):
+	log.setVerbosity(1)
+	macros = ()
+	prep = 0
+	if argSet.has_key('prep'):
+	    del argSet['prep']
+	    prep = 1
+	if argSet.has_key('macros'):
+	    argSet['macros']
+	    f = open(argSet['macros'])
+	    # XXX sick hack
+	    macroSrc = "macros =" + f.read()
+	    exec macroSrc
+	    del f
+	    del argSet['macros']
+	if argSet: return usage()
+
+	cook.cookCommand(cfg, otherArgs[2:], prep, macros)                
+    elif (otherArgs[1] == "erase"):
+	if argSet: return usage
+	if len(otherArgs) >= 3 and len(otherArgs) <=4:
+	    db = openDatabase(cfg.root, cfg.dbpath, "c")
+
+	    args = [db, cfg] + otherArgs[2:]
+	    updatecmd.doErase(*args)
+	else:
+	    return usage()
+    elif (otherArgs[1] == "import"):
+	print "this is broken"
+    elif (otherArgs[1] == "localcs"):
+	if len(otherArgs) != 4 and len(otherArgs) != 4:
+	    return usage()
+
+	name = otherArgs[2]
+	outFile = otherArgs[3]
+
+	db = database.Database(cfg.root, cfg.dbpath, "r")
+	changeset.LocalChangeSetCommand(db, cfg, name, outFile)
+    elif (otherArgs[1] == "localcommit"):
+	if len(otherArgs) < 3: return usage()
+	db = database.Database(cfg.root, cfg.dbpath, "c")
+	for changeSet in otherArgs[2:]:
+	    commit.doLocalCommit(db, changeSet)
+    elif (otherArgs[1] == "pkglist" or otherArgs[1] == "replist"):
+	all = argSet.has_key('all')
+	if all: del argSet['all']
+
+	ls = argSet.has_key('ls')
+	if ls: del argSet['ls']
+
+	if otherArgs[1] == "replist":
+	    queryRepos = openRepository(cfg.reppath, "r")
+	else:
+	    queryRepos = openDatabase(cfg.root, cfg.dbpath, "r").repcache
+
+	if argSet: return usage()
+
+	if len(otherArgs) >= 2 and len(otherArgs) <= 4:
+	    args = [queryRepos, cfg, all, ls] + otherArgs[2:]
+	    try:
+		display.displayPkgs(*args)
+	    except IOError, msg:
+		sys.stderr.write(msg.strerror + '\n')
+		sys.exit(1)
+	else:
+	    return usage()
+    elif (otherArgs[1] == "rblist"):
+	if argSet: return usage
+	db = openDatabase(cfg.root, cfg.dbpath, "r")
+	rollbacks.listRollbacks(db, cfg)
+    elif (otherArgs[1] == "rollback"):
+	if argSet: return usage
+	db = openDatabase(cfg.root, cfg.dbpath, "c")
+	args = [db, cfg] + otherArgs[2:]
+	rollbacks.apply(*args)
+    elif (otherArgs[1] == "source" or otherArgs[1] == "src"):
+	return srcctl.sourceCommand(cfg, otherArgs[2:], argSet)
+    elif (otherArgs[1] == "update"):
+	kwargs = {}
+	replaceFiles = argSet.has_key('replace-files')
+	if replaceFiles:
+	    kwargs['replaceFiles'] = True
+	    del argSet['replace-files']
+	if argSet: return usage
+	if len(otherArgs) >=3 and len(otherArgs) <= 4:
+	    repos = openRepository(cfg.reppath, "r")
+	    db = openDatabase(cfg.root, cfg.dbpath, "c")
+
+	    args = [repos, db, cfg] + otherArgs[2:]
+	    updatecmd.doUpdate(*args, **kwargs)
+	else:
+	    return usage()
+    elif (otherArgs[1] == "return usage"):
+	return usage(rc = 0)
+    else:
+	return usage()
+
+    if profile:
+	prof.stop()
+
+    if log.errorOccurred():
+	sys.exit(1)
+
+sys.exit(main())
