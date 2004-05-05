d54320843bcca0f182b2962a8d51745b35fa0fb1
msw <devnull@localhost>
2004-05-05 00:17:18 +0000

    grouping updates

--- __init__.py
+++ __init__.py
@@ -8,7 +8,8 @@ The Specifix Repository System
 
 @group Package Building: build, buildpackage, recipe, cook, policy,
                          destdirpolicy, packagepolicy
-@group Database/Repository: database, repository, versioned
+@group Command Line: cscmd, srccmd, updatecmd
+@group Database/Repository: database, local, repository, versioned
 @group Package/Changeset objects: package, changeset, versions
 @group Package installation/rollback: update, rollbacks
 @group Utility: util, lookaside, rpmhelper, sha1helper, fixedglob, log, enum
