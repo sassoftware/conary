f6e5412efe9f83984cc8ceef5b4b8d812708e11f
ewt <devnull@localhost>
2004-05-14 01:10:36 +0000

    interface for libs.elf

---
+++ deps/filedeps.py
@@ -0,0 +1,28 @@
+import deps
+import lib.elf
+
+def findFileDependencies(path, dgroup):
+    # returns two DependencySets, one for what the file requires and
+    # another for what it provides
+    results = lib.elf.inspect(path)
+
+    # the file isn't elf
+    if results == None:
+	return None
+
+    rc = []
+    
+    for depList in results:
+	set = deps.DependencySet(dgroup)
+	for (depClass, main, flags) in depList:
+	    if depClass == 'soname':
+		curClass = deps.SonameDependencies
+	    elif depClass == 'abi':
+		curClass = deps.AbiDependency
+	    else:
+		assert(0)
+
+	    set.addDep(curClass, deps.Dependency(main, flags))
+	rc.append(set)
+
+    return rc
