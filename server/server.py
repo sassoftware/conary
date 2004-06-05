7ef4d863d418012a6bd7ee74674d71e2bf010495
ewt <devnull@localhost>
2004-06-05 02:35:48 +0000

    added getChangeSet

--- server/server.py
+++ server/server.py
@@ -9,8 +9,11 @@ if len(sys.argv) != 3:
 sys.path.append(sys.argv[1])
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
 from repository import fsrepos
 import xmlshims
+import select
 
 class SRSServer(SimpleXMLRPCServer):
 
@@ -62,10 +65,38 @@ class NetworkRepositoryServer(xmlshims.NetworkConvertors,
 	return self.troveStore.troveLatestVersion(pkgName, 
 						  self.fromVersion(branchStr))
 
+    def getChangeSet(self, chgSetList, recurse, withFiles):
+	l = []
+	for (name, flavor, old, new, absolute) in chgSetList:
+	    if old == 0:
+		l.append((name, self.toFlavor(flavor), None,
+			 self.toVersion(new), absolute))
+	    else:
+		l.append((name, self.toFlavor(flavor), self.toVersion(old),
+			 self.toVersion(new), absolute))
+
+	cs = self.createChangeSet(l, recurse = recurse, withFiles = withFiles)
+	cs.writeToFile("FOO")
+	return "http://localhost:8001/FOO"
+
 netRepos = NetworkRepositoryServer(sys.argv[2], "r")
 
-server = SRSServer(("localhost", 8000))
-server.register_instance(netRepos)
-server.register_introspection_functions()
-server.serve_forever()
+xmlServer = SRSServer(("localhost", 8000))
+xmlServer.register_instance(netRepos)
+xmlServer.register_introspection_functions()
+
+httpServer = HTTPServer(("localhost", 8001), SimpleHTTPRequestHandler)
+
+fds = {}
+fds[xmlServer.fileno()] = xmlServer
+fds[httpServer.fileno()] = httpServer
+
+p = select.poll()
+for fd in fds.iterkeys():
+    p.register(fd, select.POLLIN)
+
+while True:
+    events = p.poll()
+    for (fd, event) in events:
+	fds[fd].handle_request()
 
