8256947dc055a4b27b2e7dec65a6c1bb6c7baed5
ewt <devnull@localhost>
2004-06-30 22:29:41 +0000

    1. only show items in the defaults list 2. trust the types in the defaults list for printing 3. added stringdict and repositorymap

--- conarycfg.py
+++ conarycfg.py
@@ -9,9 +9,26 @@ import deps.deps
 import os
 import versions
 
-STRING, BOOL, LABEL = range(3)
+STRING, BOOL, LABEL, STRINGDICT = range(4)
 
 class ConaryConfiguration:
+
+    defaults = {
+	'buildLabel'	        : [ LABEL,	 None ],
+	'buildPath'		: '/usr/src/conary/builds',
+	'contact'		: None,
+	'dbPath'		: '/var/lib/conarydb',
+	'debugRecipeExceptions' : [ BOOL, False ], 
+	'installLabel'		: [ LABEL,	 None ],
+	'instructionSet'	: deps.arch.current(),
+	'lookaside'		: '/var/cache/conary',
+	'name'			: None,
+	'repPath'		: '/var/lib/conary-rep',
+	'repositoryMap'	        : [ STRINGDICT, {} ],
+	'root'			: '/',
+	'sourcePath'		: '/usr/src/conary/sources',
+	'tmpDir'		: '/var/tmp/',
+    }
    
     def read(self, file):
 	if os.path.exists(file):
@@ -38,12 +55,14 @@ class ConaryConfiguration:
 
 	if type == STRING:
 	    self.__dict__[key] = val
+	elif type == STRINGDICT:
+	    (idx, val) = val.split(None, 1)
+	    self.__dict__[key][idx] = val
 	elif type == LABEL:
 	    try:
 		self.__dict__[key] = versions.BranchName(val)
 	    except versions.ParseError, e:
 		raise ParseError, str(e)
-
 	elif type == BOOL:
 	    if val.lower() in ('0', 'false'):
 		self.__dict__[key] = False
@@ -53,43 +72,32 @@ class ConaryConfiguration:
 		raise ParseError, ("%s:%s: expected True or False for configuration value '%s'" % (self.file, self.lineno, key))
 
     def display(self):
-	keys = self.__dict__.keys()
+	keys = self.defaults.keys()
 	keys.sort()
 	for item in keys:
-	    if type(self.__dict__[item]) is str:
-		print "%-20s %s" % (item, self.__dict__[item])
-	    elif isinstance(self.__dict__[item], versions.Version):
-		print "%-20s %s" % (item, self.__dict__[item].asString())
-	    elif isinstance(self.__dict__[item], versions.BranchName):
-		print "%-20s %s" % (item, self.__dict__[item].asString())
-	    elif isinstance(self.__dict__[item], deps.deps.Dependency):
-		print "%-20s %s" % (item, self.__dict__[item])
-	    elif item == "flavor":
-		pass
-            elif item == "lowerCaseMap":
-                pass
+	    if (type(self.defaults[item]) == list):
+		t = self.defaults[item][0]
 	    else:
-		print "%-20s (unknown type)" % (item)
+		t = STRING
+
+	    if t == STRING:
+		print "%-25s %s" % (item, self.__dict__[item])
+	    elif t == LABEL:
+		print "%-25s %s" % (item, self.__dict__[item].asString())
+	    elif t == STRINGDICT:
+		d = self.__dict__[item]
+		idxs = d.keys()
+		idxs.sort()
+		for idx in idxs:
+		    print "%-25s %-25s %s" % (item, idx, d[idx])
+	    elif t == BOOL:
+		print "%-25s %s" % (item, bool(self.__dict__[item]))
+	    else:
+		print "%-25s (unknown type)" % (item)
 
     def __init__(self):
-	
-	defaults = {
-	    'repPath'	     : '/var/lib/conary-rep',
-	    'root'	     : '/',
-	    'sourcePath'     : '/usr/src/conary/sources',
-	    'buildPath'	     : '/usr/src/conary/builds',
-	    'installLabel'   : [ LABEL,	 None ],
-	    'buildLabel'     : [ LABEL,	 None ],
-	    'lookaside'	     : '/var/cache/conary',
-	    'dbPath'	     : '/var/lib/conarydb',
-	    'tmpDir'	     : '/var/tmp/',
-	    'name'	     : None,
-	    'contact'	     : None,
-	    'instructionSet' : deps.arch.current(),
-	    'debugRecipeExceptions' : [ BOOL, False ] }
-	
 	self.types = {}
-	for (key, value) in defaults.items():
+	for (key, value) in self.defaults.items():
 	    if isinstance(value, (list, tuple)):
 		self.types[key] = value[0]
 		self.__dict__[key] = value[1]
