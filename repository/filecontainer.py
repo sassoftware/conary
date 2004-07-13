edbcbfd7c56260614c7758034246a40e69d4097e
ewt <devnull@localhost>
2004-07-13 18:04:29 +0000

    added licenesing terms for the CPL

--- repository/filecontainer.py
+++ repository/filecontainer.py
@@ -2,6 +2,16 @@
 # Copyright (c) 2004 Specifix, Inc.
 # All rights reserved
 #
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/cpl.php.
+#
+# This program is distributed with the whole that it will be usefull, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
 """
 Provides a file which can store multiple files inside of it.
 
