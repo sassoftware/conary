6814f65f7b1f2fd94b664b0b5ebabb5a6fc36279
johnsonm <devnull@localhost>
2004-07-19 22:50:26 +0000

    warn about excluded directories only when it is likely to be useful

--- build/packagepolicy.py
+++ build/packagepolicy.py
@@ -765,7 +765,12 @@ class ExcludeDirectories(policy.Policy):
     invariantinclusions = [ ('.*', stat.S_IFDIR) ]
 
     def doFile(self, path):
-	log.debug('excluding directory: %s', path)
+	s = os.lstat(self.recipe.macros.destdir + os.sep + path)
+	mode = s[stat.ST_MODE]
+	if mode & 0777 != 0755:
+	    log.debug('excluding directory %s with mode %o', path, mode&0777)
+	elif s[stat.ST_NLINK] == 2:
+	    log.debug('excluding empty directory %s', path)
 	del self.recipe.autopkg.pkgMap[path][path]
 	del self.recipe.autopkg.pkgMap[path]
 	del self.recipe.autopkg.pathMap[path]
