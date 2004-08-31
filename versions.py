7a7faa997f6c0b9b4a99a5cb214df42152cdf158
msw <devnull@localhost>
2004-08-31 20:53:04 +0000

    don't return the cached version if timestamps don't match

--- versions.py
+++ versions.py
@@ -798,7 +798,7 @@ def VersionFromString(ver, defaultBranch = None, timeStamps = []):
 	return NewVersion()
 
     v = stringVersionCache.get(ver, None)
-    if v is not None:
+    if v is not None and v.timeStamps() == timeStamps:
 	return v
 
     v = _VersionFromString(ver, defaultBranch, timeStamps = timeStamps)
