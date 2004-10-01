f7764e63e8fc5568a9250ab1bce31f030b1d1551
ewt <devnull@localhost>
2004-10-01 23:35:05 +0000

    set up for when we can update from a cook/emerge

--- conaryclient.py
+++ conaryclient.py
@@ -163,13 +163,16 @@ class ConaryClient:
                     # this removes duplicates
                     labels = {}.fromkeys(labels).keys()
                     
-                    # check for locally-cooked troves
-                    if True in [isinstance(x, versions.CookBranch) or
-                                isinstance(x, versions.EmergeBranch)
-                                for x in labels]:
-                        raise UpdateError, \
-                            "Package %s cooked locally, not updating" \
-                                    % troveName
+                    # updating locally cooked troves needs a label override
+                    if True or not versionStr:
+                        if True in [isinstance(x, versions.CookBranch) or
+                                    isinstance(x, versions.EmergeBranch)
+                                    for x in labels]:
+                            raise UpdateError, \
+                             "Package %s cooked locally; version, branch, or " \
+                             "label must be specified for update" % troveName
+                        labels = [ None ]
+                        
                 else:
                     labels = self.cfg.installLabelPath
 
