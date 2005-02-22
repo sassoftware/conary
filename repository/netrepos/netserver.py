44d0651aad4714033aa88da419156074765f378d
johnsonm <devnull@localhost>
2005-02-22 02:46:21 +0000

    fix toyp

--- repository/netrepos/netserver.py
+++ repository/netrepos/netserver.py
@@ -997,7 +997,7 @@ class NetworkRepositoryServer(xmlshims.NetworkConvertors):
 	for pkgCs in cs.iterNewPackageList():
 	    d = { 'reppath' : self.urlBase(),
 	    	  'trove' : pkgCs.getName(),
-                  'flavor' : deps.formatFlavor(pkCs.getFlavor()),
+                  'flavor' : deps.formatFlavor(pkgCs.getFlavor()),
 		  'version' : pkgCs.getNewVersion().asString() }
 	    cmd = self.commitAction % d
 	    os.system(cmd)
