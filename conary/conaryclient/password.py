8cdf87d6f4ce629140726958b19e77291c0792b5
Erik Troan <http://bugs.rpath.com>
2005-12-01 12:30:11 -0400

    added password.py

---
+++ conary/conaryclient/password.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2005 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/cpl.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any waranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+import getpass
+
+def getPassword(server, userName):
+
+    s = "Enter the password for %s on %s:" % (server, userName)
+    return getpass.getpass(s)
