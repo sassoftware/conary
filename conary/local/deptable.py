f84b7c0b48012456b2a48281967f30b8a9f8d48a
Cristian Gafton <http://bugs.rpath.com>
2005-12-12 16:19:36 -0400

    disable compiled statements until dbstore provides a unified interface

--- conary/local/deptable.py
+++ conary/local/deptable.py
@@ -110,6 +110,7 @@ class DependencyTables:
 
     def _populateTmpTable(self, cu, stmt, depList, troveNum, requires, 
                           provides, multiplier = 1):
+        assert(type(stmt) == type(""))
         allDeps = []
         if requires:
             allDeps += [ (False, x) for x in 
@@ -121,7 +122,7 @@ class DependencyTables:
         for (isProvides, (classId, depClass)) in allDeps:
             for dep in depClass.getDeps():
                 for (depName, flags) in zip(dep.getName(), dep.getFlags()):
-                    cu.execstmt(stmt,
+                    cu.execute(stmt,
                                    troveNum, multiplier * len(depList), 
                                     1 + len(flags), isProvides, classId, 
                                     depName, NO_FLAG_MAGIC)
@@ -131,7 +132,7 @@ class DependencyTables:
                             # prevents them from making it into any repository
                             assert("'" not in flag)
                             assert(sense == deps.FLAG_SENSE_REQUIRED)
-                            cu.execstmt(stmt,
+                            cu.execute(stmt,
                                         troveNum, multiplier * len(depList), 
                                         1 + len(flags), isProvides, classId, 
                                         depName, flag)
@@ -249,15 +250,14 @@ class DependencyTables:
     def add(self, cu, trove, troveId):
         # FIXME: this is used by the server code as well and it is
         # WAAAY too sqlite specific...
-        assert(cu.con.inTransaction)
+        #assert(cu.con.inTransaction)
         self._add(cu, troveId, trove.getProvides(), trove.getRequires())
 
     def _add(self, cu, troveId, provides, requires):
         self._createTmpTable(cu, "NeededDeps")
 
-	stmt = cu.compile("INSERT INTO NeededDeps VALUES(?, ?, ?, ?, ?, ?, ?)")
-        self._populateTmpTable(cu, stmt, [], troveId, 
-                               requires, provides)
+	stmt = "INSERT INTO NeededDeps VALUES(?, ?, ?, ?, ?, ?, ?)"
+        self._populateTmpTable(cu, stmt, [], troveId, requires, provides)
         self._mergeTmpTable(cu, "NeededDeps", "Dependencies", "Requires", 
                             "Provides", ("Dependencies",))
 
@@ -733,10 +733,11 @@ class DependencyTables:
         oldTroves = []
         troveNames = []
 
-	stmt = cu.compile("""INSERT INTO DepCheck 
-                                    (troveId, depNum, flagCount, isProvides,
-                                     class, name, flag)
-                             VALUES(?, ?, ?, ?, ?, ?, ?)""")
+	stmt = """
+        INSERT INTO DepCheck
+        (troveId, depNum, flagCount, isProvides, class, name, flag)
+        VALUES(?, ?, ?, ?, ?, ?, ?)
+        """
 
         # We build up a graph to let us split the changeset into pieces.
         # Each node in the graph represents a remove/add pair. Note that
@@ -1070,7 +1071,7 @@ class DependencyTables:
         createRequiresTable(cu, 'TmpRequires', isTemp = True)
 
         depList = [ None ]
-	stmt = cu.compile("INSERT INTO DepCheck VALUES(?, ?, ?, ?, ?, ?, ?)")
+	stmt = "INSERT INTO DepCheck VALUES(?, ?, ?, ?, ?, ?, ?)"
         for i, depSet in enumerate(depSetList):
             self._populateTmpTable(cu, stmt, depList, -i - 1, 
                                    depSet, None, multiplier = -1)
@@ -1175,7 +1176,7 @@ class DependencyTables:
         createRequiresTable(cu, 'TmpRequires', isTemp = True)
 
         depList = [ None ]
-	stmt = cu.compile("INSERT INTO DepCheck VALUES(?, ?, ?, ?, ?, ?, ?)")
+	stmt = "INSERT INTO DepCheck VALUES(?, ?, ?, ?, ?, ?, ?)"
         for i, depSet in enumerate(depSetList):
             self._populateTmpTable(cu, stmt, depList, -i - 1, 
                                    depSet, None, multiplier = -1)
