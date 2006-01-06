46650c30fb38d4f9834a99b409002abdada7e8d0
David Christian <http://bugs.rpath.com>
2006-01-06 19:37:02 -0400

    the __all__ hack is no longer necessary when __slots__ is defined

--- conary/lib/importer.py
+++ conary/lib/importer.py
@@ -62,13 +62,6 @@ def makeImportedModule(name, data, scope):
 
         def __getattr__(self, key):
             mod = _loadModule()
-            if key == '__all__':
-                # the caller tried to use __all__ to implement import *.
-                # Unforunately, that probably means the caller is
-                # an import statement which has a handle on an import
-                # object.  We can't rely on being able to overwrite that
-                # variable, so just update the proxy's dict.
-                self.__dict__.update(mod.__dict__)
             return getattr(mod, key)
 
         def __setattr__(self, key, value):
