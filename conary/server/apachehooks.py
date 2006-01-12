33d4e64f991ca5d979001751833c931592069654
David Christian <http://bugs.rpath.com>
2006-01-12 14:56:49 -0400

    add logging back to apache server

--- conary/server/apachehooks.py
+++ conary/server/apachehooks.py
@@ -318,6 +318,11 @@ def handler(req):
         if cfg.bugsFromEmail and cfg.bugsToEmail:
             exception, e, bt = sys.exc_info()
             logErrorAndEmail(req, cfg, exception, e, bt)
+        timeStamp = time.ctime(time.time())
+        log.error('[%s] Unhandled exception from conary repository: %s: %s',
+                 timeStamp, exception.__name__, e)
+        log.error(''.join(traceback.format_exception(*sys.exc_info())))
+
         return apache.HTTP_INTERNAL_SERVER_ERROR
         
 
