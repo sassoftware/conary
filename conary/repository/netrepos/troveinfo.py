3b35d7e0cea5bf2c824bb178385ec1b9dd6e45ea
Matt Wilson <http://bugs.rpath.com/>
2006-01-12 14:28:45 -0400

    remove duplicate code in local/troveinfo.py, add troveinfo.py to Makefile

--- conary/repository/netrepos/troveinfo.py
+++ conary/repository/netrepos/troveinfo.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python
 #
-# Copyright (c) 2005-2006 rPath, Inc.
+# Copyright (c) 2006 rPath, Inc.
 #
 # This program is distributed under the terms of the Common Public License,
 # version 1.0. A copy of this license should have been distributed with this
@@ -12,29 +13,8 @@
 # full details.
 #
 
-class TroveInfoTable:
+from conary.local import troveinfo
+
+class TroveInfoTable(troveinfo.TroveInfoTable):
     def __init__(self, db):
         self.db = db
-
-    def addInfo(self, cu, trove, idNum):
-        # c = True if the trove is a component
-        n = trove.getName()
-        c = ':' in n and not n.endswith(':source')
-        for (tag, (size, streamType, name)) in trove.troveInfo.streamDict.iteritems():
-            frz = trove.troveInfo.__getattribute__(name).freeze()
-            if frz:
-                # FIXME: somehow we're getting buildReqs and loadedTrovs in the
-                # troveInfo table for components.  prevent this until the bug
-                # is found.
-                if c and (tag == 4 or tag == 5):
-                    raise RuntimeError('attempted to add build requires '
-                                       'trove info for a component: %s' %n)
-                cu.execute("INSERT INTO TroveInfo (instanceId, infoType, data) "
-                           "VALUES (?, ?, ?)", (idNum, tag, cu.binary(frz)))
-
-    def getInfo(self, cu, trove, idNum):
-        cu.execute("SELECT infoType, data FROM TroveInfo WHERE instanceId=?",
-                   idNum)
-        for (tag, frz) in cu:
-            name = trove.troveInfo.streamDict[tag][2]
-            trove.troveInfo.__getattribute__(name).thaw(cu.frombinary(frz))
