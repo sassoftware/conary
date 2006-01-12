73c4df7b6aa1a839e427f8cf2ad520f46e09f763
Matt Wilson <http://bugs.rpath.com/>
2006-01-12 14:09:15 -0400

    update copyrights

--- conary/build/loadrecipe.py
+++ conary/build/loadrecipe.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2005 rPath, Inc.
+# Copyright (c) 2004-2006 rPath, Inc.
 #
 # This program is distributed under the terms of the Common Public License,
 # version 1.0. A copy of this license should have been distributed with this
