cf9807435b659828b4c8aa1804156616773f2505
David Christian <http://bugs.rpath.com>
2006-03-13 09:16:19 -0400

    use optparse to do option parsing instead of hand parsing.

--- conary/lib/options.py
+++ conary/lib/options.py
@@ -15,10 +15,11 @@
 Command-line option handling
 """
 
+import optparse
+import StringIO
+
 from conary.lib import log, util
 import sys
-
-
 
 
 (NO_PARAM,   # arg may occur, no parameter
@@ -28,10 +29,27 @@ import sys
  ) = range(0,4)
 
 class OptionError(Exception):
-    
     val = 1
 
+class OptionParser(optparse.OptionParser):
+    def error(self, msg):
+        raise OptionError(msg)
+
+def optParamCallback(option, opt_str, value, parser, *args, **kw):
+    value = True
+    if parser.rargs:
+        potentialParam = parser.rargs[0]
+        if not potentialParam:
+            del parser.rargs[0]
+        elif potentialParam[0] != '-':
+            value = potentialParam 
+            del parser.rargs[0]
+    setattr(parser.values, option.dest, value)
+
 def processArgs(argDef, cfgMap, cfg, usage, argv=sys.argv):
+    """Backwards-compatible (with earlier conary processArgs)
+       function that uses optparse as its backend.
+    """
     otherArgs = [ argv[0] ]
     argSet = {}
     # don't mangle the command line
@@ -42,79 +60,42 @@ def processArgs(argDef, cfgMap, cfg, usage, argv=sys.argv):
     argDef['debug'] = NO_PARAM
     argDef['debugger'] = NO_PARAM
 
-    i = 1
-    while i < len(argv):
-	if argv[i][:2] != "--":
-	    otherArgs.append(argv[i])
-        # stop processing args after --
-        elif argv[i] == '--':
-                otherArgs.extend(argv[i+1:])
-                break
-	else:
-            arg = argv[i][2:]
-            arg_parts = arg.split('=', 1)
-            if len(arg_parts) > 1:
-                arg = arg_parts[0]
-                # don't allow --foo=bar arg if foo doesn't exist
-                # or doesn't take an arg.
-                if not argDef.has_key(arg):
-                    usage()
-                    raise OptionError("Unknown Flag '%s'r" % arg)
-                elif argDef[arg] == NO_PARAM:
-                    usage()
-                    raise OptionError(
-                                "Flag '%s' does not take a parameter" % arg)
-                argv[i] = arg
-                argv.insert(i+1, arg_parts[1])
-	    if not argDef.has_key(arg): 
-                usage()
-                raise OptionError("Unknown flag '%s'" % arg)
+    # historically, usage was generally a function to print out the usage 
+    # message.  We want it to be a string.  For now, we
+    # convert here to allow backwards compatibility.
+    s = StringIO.StringIO()
+    oldStdOut = sys.stdout
+    sys.stdout = s
+    try:
+        usage()
+    except SystemExit:
+        # some of these old usage functions even exit after 
+        # printing the usage message!
+        pass
+    sys.stdout = oldStdOut
+    usage = s.getvalue()
 
-	    if argDef[arg] == NO_PARAM:
-		argSet[arg] = True
-	    elif argDef[arg] == OPT_PARAM:
-		# max one setting
-		if argSet.has_key(arg): 
-                    raise OptionError(
-                            "Flag '%s' takes at most one parameter" % arg)
-		if i >= len(argv): 
-		    argSet[arg] = True
-                else:
-                    if i + 1 < len(argv):
-                        next_arg = argv[i+1]
-                    else:
-                        # option was last on the command line,
-                        # and no optional paramater was given
-                        next_arg = ''
-                    if next_arg == '':
-                        argSet[arg] = True
-                        i = i + 1
-                    elif next_arg[0:2] == '--': 
-                        argSet[arg] = True
-                    else:
-                        argSet[arg] = next_arg
-                        i = i + 1
-	    else:
-		# the argument takes a parameter
-		i = i + 1
-		if i >= len(argv): 
-                    usage()
-                    raise OptionError("Flag '%s' requires a parameter" % arg)
+    parser = OptionParser(usage=usage, add_help_option=False)
 
-		if argDef[arg] == ONE_PARAM:
-		    # exactly one parameter is allowd
-		    if argSet.has_key(arg): 
-                        usage()
-                        raise OptionError(
-                            "Flag '%s' requires exactly one parameter" % arg)
-		    argSet[arg] = argv[i]
-		else:
-		    # multiple parameters may occur
-		    if argSet.has_key(arg):
-			argSet[arg].append(argv[i])
-		    else:
-			argSet[arg] = [argv[i]]
-	i = i + 1
+    for name, paramType in argDef.iteritems():
+        if paramType == NO_PARAM:
+            parser.add_option('--' + name, action='store_true', dest=name)
+        elif paramType == ONE_PARAM:
+            parser.add_option('--' + name, dest=name)
+        elif paramType == OPT_PARAM:
+            parser.add_option('--' + name, action='callback',
+                               callback=optParamCallback, dest=name,
+                               type='string', nargs=0)
+        elif paramType == MULT_PARAM:
+            parser.add_option('--' + name, action='append', dest=name)
+
+    (options, otherArgs) = parser.parse_args(argv)
+    argSet = {}
+    for name in argDef:
+        val = getattr(options, name)
+        if val is None:
+            continue
+        argSet[name] = val
 
     if 'config-file' in argSet:
         try:
