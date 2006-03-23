cdab19cde151cab29d2913a25a60e706a5cb8aa9
David Christian <http://bugs.rpath.com>
2006-03-23 17:08:59 -0400

    make sure paths are normalized

--- scripts/perlreqs.pl
+++ scripts/perlreqs.pl
@@ -7,5 +7,9 @@ $map = scan_deps(files=>[$ARGV[0]], recurse => 0);
 # processing in Python.  We use // as a separator because it will
 # never be found within a normalized POSIX path.
 foreach $item (values %$map) {
-    print $item->{type} . "//" . $item->{file} . "//" . $item->{key} . "\n";
+    # since we depend on the path being normalized, make sure they
+    # are!
+    $file = $item->{file};
+    $file = s/\/\/+/\//g;
+    print $item->{type} . "//" . $file . "//" . $item->{key} . "\n";
 }
