764fb0eb61ca6501b87ca54b91e8cbb44d71bdab
Jonathan Smith <http://bugs.rpath.com/>
2006-07-10 11:51:21 -0400

    don't bother running automake if autoconf has already failed in r.Automake()

--- conary/build/build.py
+++ conary/build/build.py
@@ -365,9 +365,9 @@ class Automake(BuildCommand):
     and C{--foreign} arguments to the C{automake} program.
     """
     # note: no use of %(args)s -- to which command would it apply?
-    template = ('cd %%(actionDir)s; '
-                'aclocal %%(m4DirArgs)s %(aclocalArgs)s; '
-		'%(preAutoconf)s autoconf %(autoConfArgs)s; '
+    template = ('cd %%(actionDir)s && '
+                'aclocal %%(m4DirArgs)s %(aclocalArgs)s && '
+		'%(preAutoconf)s autoconf %(autoConfArgs)s && '
 		'automake%(automakeVer)s %(autoMakeArgs)s')
     keywords = {'autoConfArgs': '',
                 'autoMakeArgs': '',
