a9235b862f322af44589d761e333fefd363ce130
Erik Troan <http://bugs.rpath.com>
2006-07-19 21:39:40 -0400

    don't let update jobs contain removed troves

--- conary/errors.py
+++ conary/errors.py
@@ -91,3 +91,13 @@ class ShadowRedirect(ConaryError):
 
     def __init__(self, n, v, f):
         self.info = (n, v, f)
+
+class RemovedTrovesError(ConaryError):
+
+    def __str__(self):
+        return "The following troves no longer exist in the repository and " \
+               "cannot be installed: %s" % \
+               ", ".join([ "%s=%s[%s]" % x for x in self.l ])
+
+    def __init__(self, troveList):
+        self.l = troveList
