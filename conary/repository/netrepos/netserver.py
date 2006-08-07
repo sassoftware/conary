c777e3814553931d69e65e9d7bf7087a33f35b76
Matt Wilson <https://issues.rpath.com/>
2006-08-07 13:03:39 -0400

    invalidate cache entries when mirroring signatures
        * The Conary repository server now invalidates changeset cache
          entries when adding a digital signature to a previously mirrored
          trove.

--- conary/repository/netrepos/netserver.py
+++ conary/repository/netrepos/netserver.py
@@ -2057,6 +2057,8 @@ class NetworkRepositoryServer(xmlshims.NetworkConvertors):
                 WHERE infoType = ? AND instanceId = ?
                 """, (sig, trove._TROVEINFO_TAG_SIGS, instanceId))
                 updateCount += 1
+            self.cache.invalidateEntry(name, self.toVersion(version),
+                                       self.toFlavor(flavor))
 
         return updateCount
 
