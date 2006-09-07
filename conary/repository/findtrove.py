d09e394c995a2bc46f476dfb25957de378456d49
David Christian <http://issues.rpath.com>
2006-09-06 23:37:53 -0400

    update support for passing in versions/branches into findTrove

--- conary/repository/findtrove.py
+++ conary/repository/findtrove.py
@@ -661,7 +661,7 @@ class TroveFinder:
             hasLabelPath = False
 
             # need a branch or a full label
-            if versionStr and (versionStr[0] == '/' or '@' in versionStr[1:]):
+            if versionStr and (isinstance(versionStr, (versions.Branch, versions.Version)) or versionStr[0] == '/' or '@' in versionStr[1:]):
                 hasLabelPath = True
 
             if not hasLabelPath and not self.allowNoLabel:
