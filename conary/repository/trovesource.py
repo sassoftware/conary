714acfb480da599d0d108f47f589a4f2e2b91090
Erik Troan <http://bugs.rpath.com>
2006-09-20 11:17:01 -0400

    1. switch StreamCollection to thaw lazily -- this avoids a thaw/freeze for    BuildRequires, which are large and never used 2. removed the troveCache from ChangeSetFileTroveSource. It got huge, and with    the other changes on this branch it is unnecessary :-)

--- conary/repository/trovesource.py
+++ conary/repository/trovesource.py
@@ -532,7 +532,6 @@ class ChangesetFilesTroveSource(SearchableTroveSource):
         self.rooted = {}
         self.idMap = {}
         self.storeDeps = storeDeps
-        self._troveCache = {}
 
         if storeDeps:
             self.depDb = deptable.DependencyDatabase()
@@ -689,12 +688,6 @@ class ChangesetFilesTroveSource(SearchableTroveSource):
             if info not in self.troveCsMap:
                 retList.append(None)
                 continue
-            # NOTE: Turned off troveCache - turning it on took up an
-            # extra 75M when grabbing all troves out of a changeset for
-            # group-dist.
-            #if info, withFiles in self._troveCache:
-            #    retList.append(self._troveCache[info, withFiles])
-            #    continue
 
             trvCs = self.troveCsMap[info].getNewTroveVersion(*info)
             if trvCs.getOldVersion() is None:
@@ -712,7 +705,6 @@ class ChangesetFilesTroveSource(SearchableTroveSource):
                 newTrove.applyChangeSet(trvCs,
                                         skipFiles = not withFiles,
                                         skipIntegrityChecks = not withFiles)
-            #self._troveCache[info, withFiles] = newTrove
             retList.append(newTrove)
 
         return retList
