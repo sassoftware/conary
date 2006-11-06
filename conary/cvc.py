7488d2af7a9fb2906970a26dd7cf79586dcbfa8b
Mihai Ibanescu <https://issues.rpath.com/>
2006-11-06 11:12:08 -0500

    Implemented cvc --logfile with support for stdin. (CNY-937)
    --message and --logfile are mutually exclusive.
    One can pass --logfile - to read the checkin message from standard input.

--- conary/cvc.py
+++ conary/cvc.py
@@ -226,13 +226,17 @@ class CommitCommand(CvcCommand):
     commandGroup = 'Repository Access'
     docs = {'message':'Use MESSAGE to describe why the commit was performed',
             'test':   ('Runs through all the steps of committing but does not '
-                       'modify the repository')}
+                       'modify the repository'),
+            'logfile': 'Read the commit message from file LOGFILE (use - for '
+                       'standard input)',
+    }
     # allow "cvc commit -m'foo bar'" to work
     hobbleShortOpts = False
     def addParameters(self, argDef):
         CvcCommand.addParameters(self, argDef)
         argDef["message"] = '-m', ONE_PARAM
         argDef["test"] = NO_PARAM
+        argDef["logfile"] = '-l', ONE_PARAM
 
     def runCommand(self, cfg, argSet, args, profile = False, 
                    callback = None, repos = None):
@@ -240,9 +244,27 @@ class CommitCommand(CvcCommand):
         level = log.getVerbosity()
         message = argSet.pop("message", None)
         test = argSet.pop("test", False)
+        logfile = argSet.pop("logfile", None)
         sourceCheck = True
 
         if argSet or len(args) != 1: return self.usage()
+
+        if message and logfile:
+            raise errors.ConaryError("options --message and --logfile are "
+                "mutually exclusive")
+        if logfile:
+            # Read the checkin message from the file
+            if logfile == '-':
+                message = sys.stdin.read()
+            else:
+                try:
+                    message = open(logfile).read()
+                except IOError, e:
+                    raise errors.ConaryError("While opening %s: %s" % (
+                        e.filename, e.strerror))
+            # Get rid of trailing white spaces, they're probably not 
+            # intended to be there anyway
+            message = message.rstrip()
 
         checkin.commit(repos, cfg, message, callback=callback, test=test)
         log.setVerbosity(level)
