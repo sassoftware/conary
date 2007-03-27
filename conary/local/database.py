6ffbc2722b966d94ab791ba59226a116e2d9bc86
Mihai Ibanescu <https://issues.rpath.com/>
2007-03-27 10:40:01 -0400

    Release the lock on the database (CNY-1455)

--- conary/local/database.py
+++ conary/local/database.py
@@ -973,6 +973,8 @@ class Database(SqlDbRepository):
         del newErrs, dbConflicts
 
         if errList:
+            # make sure we release the lock on the database
+            self.db.rollback()
             raise CommitError, ('applying update would cause errors:\n' + 
                                 '\n\n'.join(str(x) for x in errList))
         if test:
