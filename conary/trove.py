f2fd6de3fa62ff9ce92cd0569922db936172a459
Matt Wilson <https://issues.rpath.com/>
2007-03-30 15:22:41 -0400

    always set the sha256 digest of the frozen metadata needed for signatures

--- conary/trove.py
+++ conary/trove.py
@@ -349,6 +349,14 @@ class VersionedSignaturesSet(streams.StreamCollection):
 
         raise KeyError(version)
 
+    def getDigest(self, version = 0):
+        for vds in self.getStreams(1):
+            if version == vds.version():
+                digest = vds.digest()
+                return digest
+
+        return None
+
     def addDigest(self, digest, version = 0):
         vds = VersionedDigitalSignatures()
         vds.version.set(version)
@@ -519,6 +527,8 @@ _METADATA_ITEM_TAG_BIBLIOGRAPHY = 7
 _METADATA_ITEM_TAG_SIGNATURES = 8
 _METADATA_ITEM_TAG_NOTES = 9
 
+_METADATA_ITEM_SIG_VER_ALL = [ 0 ]
+
 class MetadataItem(streams.StreamSet):
     streamDict = {
         _METADATA_ITEM_TAG_ID:
@@ -562,9 +572,15 @@ class MetadataItem(streams.StreamSet):
         digest.compute(frz)
         self.id.set(digest())
 
-    def addDigitalSignature(self, keyId):
-        self.signatures.addDigest(self._digest(0), 0)
-        self.signatures.sign(keyId, 0)
+    def _updateDigests(self):
+        self._updateId()
+        for version in _METADATA_ITEM_SIG_VER_ALL:
+            if not self.signatures.getDigest(version):
+                self.signatures.addDigest(self._digest(version), version)
+
+    def addDigitalSignature(self, keyId, version=0):
+        self._updateDigests()
+        self.signatures.sign(keyId, version)
 
     def verifyDigitalSignatures(self, serverName=None):
         keyCache = openpgpkey.getKeyCache()
@@ -591,7 +607,7 @@ class MetadataItem(streams.StreamSet):
         return missingKeys, badFingerprints
 
     def freeze(self, *args, **kw):
-        self._updateId()
+        self._updateDigests()
         return streams.StreamSet.freeze(self, *args, **kw)
 
 class Metadata(streams.StreamCollection):
