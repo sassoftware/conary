46718e7cfd9680663d17a3435bc091cd3f90687d
Jonathan Smith <https://issues.rpath.com/>
2007-05-08 15:00:40 -0400

    reference the correct url for the CPL license (CNY-1637)

--- conary/lib/misc.c
+++ conary/lib/misc.c
@@ -5,7 +5,7 @@
  * This program is distributed under the terms of the Common Public License,
  * version 1.0. A copy of this license should have been distributed with this
  * source file in a file called LICENSE. If it is not present, the license
- * is always available at http://www.opensource.org/licenses/cpl.php.
+ * is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
  *
  * This program is distributed in the hope that it will be useful, but
  * without any warranty; without even the implied warranty of merchantability
