eb58aafec998e2b60d1a0252488606524d2be8d1
Mihai Ibanescu <https://issues.rpath.com/>
2007-06-08 17:21:13 -0400

    Some pyflakes cleanup

--- conary/build/derivedpolicy.py
+++ conary/build/derivedpolicy.py
@@ -15,7 +15,7 @@
 import re, os
 
 from conary import files, trove
-from conary.build import buildpackage, destdirpolicy, filter, packagepolicy, policy
+from conary.build import destdirpolicy, filter, packagepolicy, policy
 from conary.deps import deps
 
 FixDirModes = destdirpolicy.FixDirModes
