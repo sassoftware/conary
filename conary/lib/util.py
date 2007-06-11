618fb6fc8e354020127ae0a983dc39690544a60a
Jonathan Smith <https://issues.rpath.com/>
2007-06-11 16:15:40 -0400

    use subprocess.call instead of subprocess.Popen

--- conary/lib/util.py
+++ conary/lib/util.py
@@ -239,8 +239,7 @@ def execute(cmd, destDir=None, verbose=True):
         dir = destdir
     else:
         dir = '.'
-    p = subprocess.Popen('cd \'%s\'; %s' %(dir, cmd), shell=True, stdin=open(os.devnull, 'r'))
-    rc = os.waitpid(p.pid, 0)[1]
+    rc = subprocess.call('cd \'%s\'; %s' %(dir, cmd), shell=True, stdin=open(os.devnull, 'r'))
     _handle_rc(rc, cmd)
 
 class popen:
