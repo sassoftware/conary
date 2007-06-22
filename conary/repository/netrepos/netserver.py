58bb5b814ae0c57d92a382819ff8cab292b30ea3
Cristian Gafton <http://issues.rpath.com>
2007-06-22 00:35:38 -0400

    update getNewSigList for UserGroupInstancesCache

--- conary/repository/netrepos/netserver.py
+++ conary/repository/netrepos/netserver.py
@@ -2589,42 +2589,29 @@ class NetworkRepositoryServer(xmlshims.NetworkConvertors):
         # Since signatures are small blobs, it doesn't make a lot
         # of sense to use a LIMIT on this query...
         query = """
-        SELECT UP.permittedTrove, item, version, flavor, Instances.changed
+        SELECT distinct item, version, flavor, Instances.changed
         FROM Instances
         JOIN TroveInfo USING (instanceId)
-        JOIN Nodes ON
-             Instances.itemId = Nodes.itemId AND
-             Instances.versionId = Nodes.versionId
-        JOIN LabelMap ON
-             Nodes.itemId = LabelMap.itemId AND
-             Nodes.branchId = LabelMap.branchId
-        JOIN (SELECT
-                  Permissions.labelId as labelId,
-                  PerItems.item as permittedTrove,
-                  Permissions.permissionId as aclId
-              FROM Permissions
-              JOIN UserGroups ON Permissions.userGroupId = userGroups.userGroupId
-              JOIN Items AS PerItems ON Permissions.itemId = PerItems.itemId
-              WHERE Permissions.userGroupId in (%s)
-                AND UserGroups.canMirror = 1
-             ) as UP ON ( UP.labelId = 0 or UP.labelId = LabelMap.labelId )
+        JOIN UserGroupInstancesCache as ugi ON
+            Instances.instanceId = ugi.instanceId
+        JOIN UserGroups ON
+            ugi.userGroupId = UserGroups.userGroupId AND
+            UserGroup.canMirror = 1
         JOIN Items ON Instances.itemId = Items.itemId
         JOIN Versions ON Instances.versionId = Versions.versionId
         JOIN Flavors ON Instances.flavorId = flavors.flavorId
-        WHERE Instances.changed <= ?
-          AND Instances.isPresent = ?
+        WHERE ugi.userGroupId in (%s)
+          AND Instances.changed <= ?
+          AND Instances.isPresent = %d
           AND TroveInfo.changed >= ?
-          AND TroveInfo.infoType = ?
+          AND TroveInfo.infoType = %d
         ORDER BY TroveInfo.changed
-        """ % (",".join("%d" % x for x in userGroupIds), )
-        cu.execute(query, (mark, instances.INSTANCE_PRESENT_NORMAL, mark,
-                           trove._TROVEINFO_TAG_SIGS))
-
-        l = set()
-        for pattern, name, version, flavor, mark in cu:
-            if self.auth.checkTrove(pattern, name):
-                l.add((mark, (name, version, flavor)))
-        return list(l)
+        """ % (",".join("%d" % x for x in userGroupIds),
+               instances.INSTANCE_PRESENT_NORMAL, trove._TROVEINFO_TAG_SIGS)
+        # the fewer query parameters passed in, the better PostgreSQL optimizes the query
+        # so we embed the constants in the query and bind the user supplied data
+        cu.execute(query, (mark, mark))
+        return [ (m, (n,v,f)) for n,v,f,m in cu ]
 
     @accessReadOnly
     def getNewTroveInfo(self, authToken, clientVersion, mark, infoTypes,
