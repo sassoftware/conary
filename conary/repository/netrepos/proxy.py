5be89c6a5778c51389a138d49b9d03916c1f4414
Mihai Ibanescu <https://issues.rpath.com/>
2007-11-26 12:32:31 -0500

    Make sure we're not marshalling methods that start with _ (CNY-2289)

--- conary/repository/netrepos/proxy.py
+++ conary/repository/netrepos/proxy.py
@@ -100,6 +100,9 @@ class ProxyCaller:
                          self._transport.responseProtocol)
 
     def __getattr__(self, method):
+        # Don't invoke methods that start with _
+        if method.startswith('_'):
+            raise AttributeError(method)
         return lambda *args, **kwargs: self.callByName(method, *args, **kwargs)
 
     def __init__(self, url, proxy, transport):
@@ -170,6 +173,9 @@ class RepositoryCaller:
         return None
 
     def __getattr__(self, method):
+        # Don't invoke methods that start with _
+        if method.startswith('_'):
+            raise AttributeError(method)
         return lambda *args, **kwargs: self.callByName(method, *args, **kwargs)
 
     def __init__(self, protocol, port, authToken, repos, remoteIp, rawUrl,
