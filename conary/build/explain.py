530ed66d1eac3a0edd0cdfd2d0523a5f4721e1ab
Matt Wilson <https://issues.rpath.com/>
2007-12-11 14:25:55 -0500

    extend cvc explain functionality
    add explain output for group, redirect, and info recipes.
    error of the method is defined in more than one class
    add a way to disambiguate on the command line by specifying the base class
    print the class name when displaying the documentation

--- conary/build/explain.py
+++ conary/build/explain.py
@@ -14,12 +14,14 @@
 import sys
 import re
 import pydoc, types
+from conary import versions
 from conary.build import source
-from conary.build import packagerecipe
+from conary.build import packagerecipe, redirectrecipe, derivedrecipe
+from conary.build import filesetrecipe, grouprecipe, inforecipe
 
 DELETE_CHAR = chr(8)
 
-def formatString(msg):
+def _formatString(msg):
     if msg[0] == 'B':
         res = ''
         skipIndex = 0
@@ -35,7 +37,7 @@ def formatString(msg):
     else:
         return msg[2:-1]
 
-def formatDoc(obj):
+def _formatDoc(className, obj):
     name = obj.__name__
     docString = obj.__doc__
     if not docString:
@@ -44,33 +46,104 @@ def formatDoc(obj):
     srch = re.search(docStringRe, docString)
     while srch:
         oldString = srch.group()
-        newString = formatString(oldString)
+        newString = _formatString(oldString)
         docString = docString.replace(oldString, newString)
         srch = re.search(docStringRe, docString)
     # pydoc is fooled by conary's wrapping of stdout. override it if needed.
     if sys.stdout.isatty():
         pydoc.pager = lambda x: pydoc.pipepager(x, 'less')
-    pydoc.pager("Conary API Documentation: %s\n\n" % formatString('B{' + name + '}') + docString)
+    pydoc.pager("Conary API Documentation: %s\n\n" %
+                _formatString('B{%s.%s}' %(className, name)) + docString)
+
+def _parentName(klass):
+    return klass.__base__.__name__
 
 def docObject(cfg, what):
-    class DummyRecipe(packagerecipe.PackageRecipe):
+    class DummyRepos:
+        def __getattr__(self, what):
+            def f(*args, **kw):
+                return True
+            return f
+
+    class DummyPackageRecipe(packagerecipe.PackageRecipe):
         def __init__(self, cfg):
             self.name = 'package'
             self.version = '1.0'
             packagerecipe.PackageRecipe.__init__(self, cfg, None, None)
-    r = DummyRecipe(cfg)
-    r._loadSourceActions(lambda x: True)
-    r.loadPolicy()
-    try:
+            self._loadSourceActions(lambda x: True)
+            self.loadPolicy()
+
+    class DummyGroupRecipe(grouprecipe.GroupRecipe):
+        def __init__(self, cfg):
+            self.name = 'group-dummy'
+            self.version = '1.0'
+            troveCache = grouprecipe.TroveCache(None, None)
+            repos = DummyRepos()
+            grouprecipe.GroupRecipe.__init__(self, repos, cfg,
+                                             versions.Label('a@b:c'), None,
+                                             None)
+
+    class DummyRedirectRecipe(redirectrecipe.RedirectRecipe):
+        def __init__(self, cfg):
+            self.name = 'redirect'
+            self.verison = '1.0'
+            redirectrecipe.RedirectRecipe.__init__(self, None, cfg, None, None)
+
+## unused - too much overlap with PackageRecipe
+##  class DummyDerivedRecipe(derivedrecipe.DerivedPackageRecipe):
+##      def __init__(self, cfg):
+##          self.name = 'derived'
+##          self.version = '1.0'
+##          derivedrecipe.DerivedPackageRecipe.__init__(self, cfg, None, None)
+
+    class DummyInfoRecipe(inforecipe.UserGroupInfoRecipe):
+        def __init__(self, cfg):
+            self.name = 'info-dummy'
+            self.version = '1.0'
+            inforecipe.UserGroupInfoRecipe.__init__(self, cfg, None, None)
+
+    # see if a parent class was specified (to disambiguate)
+    className = None
+    if '.' in what:
+        split = what.split('.')
+        if len(split) != 2:
+            print 'Too may "." specified in "%s"' %(what)
+            return 1
+        className, what = split
+
+    classList = [ DummyPackageRecipe, DummyGroupRecipe, DummyRedirectRecipe,
+                  DummyInfoRecipe ]
+    # filter out by the parent class specified
+    if className:
+        classList = [ x for x in classList if _parentName(x) == className ]
+
+    # start looking for the object that implements the method
+    found = []
+    for klass in classList:
+        r = klass(cfg)
+        if not hasattr(r, what):
+            continue
+
         obj = getattr(r, what)
+        # The dynamic policy loader stores references to the
+        # actual object or class in variables of _recipeHelper
+        # and _policyUpdater classes.  This will pull the actual
+        # class from those instances so we can inspect the docstring
         if hasattr(obj, 'theobject'):
             obj = obj.theobject
         elif hasattr(obj, 'theclass'):
             obj = obj.theclass
         if isinstance(obj, types.InstanceType):
             obj = obj.__class__
-        formatDoc(obj)
-    except AttributeError, e:
+        found.append((klass, obj))
+
+    if len(found) == 1:
+        _formatDoc(_parentName(found[0][0]), found[0][1])
+        return 0
+    elif len(found) > 1:
+        print ('Ambiguous recipe method "%s" is defined by %s'
+               % (what, ', '.join(_parentName(x[0]) for x in found)))
+        return 1
+    else:
         print 'Unknown recipe method "%s"' %what
         return 1
-
