16240e3dbdc3ed5ae7cc9ee245cca62b88b5a133
Stu Gott <http://issues.rpath.com/>
2008-05-27 17:01:09 -0400

    make info recipes use policy (CNY-2723)

--- conary/build/inforecipe.py
+++ conary/build/inforecipe.py
@@ -15,9 +15,8 @@
 from conary.build.packagerecipe import AbstractPackageRecipe, _recipeHelper
 from conary.build.recipe import RECIPE_TYPE_INFO
 
-from conary.build import buildpackage
+from conary.build import policy
 from conary.build import usergroup
-from conary.deps import deps
 
 class UserGroupInfoRecipe(AbstractPackageRecipe):
     _recipeType = RECIPE_TYPE_INFO
@@ -25,53 +24,7 @@ class UserGroupInfoRecipe(AbstractPackageRecipe):
     # we need to add this line because AbstractPackageRecipe
     # isn't copied in
     buildRequires = AbstractPackageRecipe.buildRequires[:]
-
-    def __init__(self, cfg, laReposCache, srcdirs, extraMacros={}, 
-                 crossCompile=None):
-        AbstractPackageRecipe.__init__(self, cfg, laReposCache, srcdirs, extraMacros, crossCompile)
-        self.requires = []
-        self.infofilename = None
-        self.realfilename = None
-
-    def getPackages(self):
-        # we do not package up build logs for info-* packages
-        self._autoCreatedFileCount -= 2
-        comp = buildpackage.BuildComponent(
-            'info-%s:%s' %(self.infoname, self.type), self)
-        f = comp.addFile(self.infofilename, self.realfilename)
-        f.tags.set("%s-info" %self.type)
-        self.addProvides(f)
-        self.addRequires(f)
-        comp.provides.union(f.provides())
-        comp.requires.union(f.requires())
-        return [comp]
-
-    def loadPolicy(self):
-        return []
-
-    def doProcess(self, bucket, logFile = None):
-        pass
-
-    def addProvides(self, f):
-        pass
-
-    def addRequires(self, f):
-        if not self.requires:
-            return
-        depSet = deps.DependencySet()
-        for info, type in self.requires:
-            if type == 'user':
-                depClass = deps.UserInfoDependencies
-            else:
-                depClass = deps.GroupInfoDependencies
-            depSet.addDep(depClass, deps.Dependency(info, []))
-        f.requires.set(depSet)
-
-    def requiresUser(self, user):
-        self.requires.append((user, 'user'))
-
-    def requiresGroup(self, group):
-        self.requires.append((group, 'group'))
+    basePolicyClass = policy.UserGroupBasePolicy
 
     def __getattr__(self, name):
         if not name.startswith('_'):
@@ -80,27 +33,76 @@ class UserGroupInfoRecipe(AbstractPackageRecipe):
                                      usergroup.__dict__[name])
         if name in self.__dict__:
             return self.__dict__[name]
-        raise AttributeError, name
+        return AbstractPackageRecipe.__getattr__(self, name)
+
+    def _loadSourceActions(self, *args, **kwargs):
+        pass
+
+    def loadPolicy(self):
+        return AbstractPackageRecipe.loadPolicy(self,
+                                internalPolicyModules = ( 'infopolicy', ) )
 
 class UserInfoRecipe(UserGroupInfoRecipe):
-    type = 'user'
-    internalAbstractBaseClass = 1
+    """
+    NAME
+    ====
+    B{C{UserInfoRecipe}} - Build user info pacakges
 
-    def addProvides(self, f):
-        depSet = deps.DependencySet()
-        depSet.addDep(deps.UserInfoDependencies,
-                      deps.Dependency(self.infoname, []))
-        if self.provideGroup:
-            depSet.addDep(deps.GroupInfoDependencies,
-                          deps.Dependency(self.groupname, []))
-        f.provides.set(depSet)
+    SYNOPSIS
+    ========
+
+    C{UserInfoRecipe} is used to create packages that define a system user
+
+    DESCRIPTION
+    ===========
+
+    The C{UserInfoRecipe} class provides an interface to define a system
+    user through the C{r.User} method.  The C{r.User} method is also
+    available in the C{PackageRecipe} class.
+
+    EXAMPLE
+    =======
+    A sample class that uses C{UserInfoRecipe} to define a user
+
+        class ExamplePackage(UserInfoRecipe):
+            name = 'info-example'
+            version = '1.0'
+
+            def setup(r):
+                r.User('example', 500)
+    """
+    internalAbstractBaseClass = 1
 
 class GroupInfoRecipe(UserGroupInfoRecipe):
-    type = 'group'
-    internalAbstractBaseClass = 1
+    """
+    NAME
+    ====
+    B{C{GroupInfoRecipe}} - Build group info pacakges
 
-    def addProvides(self, f):
-        depSet = deps.DependencySet()
-        depSet.addDep(deps.GroupInfoDependencies,
-                      deps.Dependency(self.infoname, []))
-        f.provides.set(depSet)
+    SYNOPSIS
+    ========
+
+    C{GroupInfoRecipe} is used to create packages that define a system group
+
+    DESCRIPTION
+    ===========
+
+    The C{GroupInfoRecipe} class provides an interface to define a system
+    group through the C{r.Group} method.  The C{r.Group} method is also
+    available in the C{PackageRecipe} class.
+
+    The C{GroupInfoRecipe} class should be used if a system group must exist
+    independently from any system users.
+
+    EXAMPLE
+    =======
+    A sample class that uses C{GroupInfoRecipe} to define a group
+
+        class ExamplePackage(GroupInfoRecipe):
+            name = 'info-example'
+            version = '1.0'
+
+            def setup(r):
+                r.Group('example', 500)
+    """
+    internalAbstractBaseClass = 1
