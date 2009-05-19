27356aef822e63cbe517da16fceed821c1bd0b18
Michael K. Johnson <http://issues.rpath.com/>
2009-05-19 15:48:16 -0400

    libtool .la files in :devellib (CNY-3143)

--- config/components/base/devellib.py
+++ config/components/base/devellib.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2004-2008 rPath, Inc.
+# Copyright (c) 2004-2009 rPath, Inc.
 #
 # This program is distributed under the terms of the Common Public License,
 # version 1.0. A copy of this license should have been distributed with this
@@ -19,6 +19,7 @@
 import stat
 filters = ('devellib', ((r'\.so', stat.S_IFLNK),
                         r'\.a',
+                        r'\.la', # CNY-3143
                         '(%(libdir)s|%(datadir)s)/pkgconfig/'))
 follows = ('perl', 'python')
 precedes = ('lib', 'devel', 'data')
