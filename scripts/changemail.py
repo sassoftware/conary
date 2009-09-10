384ba4804c6f10af19179cc66b0cf2fd3abf2942
Michael K. Johnson <http://issues.rpath.com/>
2009-09-09 22:16:22 -0400

    changemail module now takes optional --maxsize argument (CNY-3205)

--- scripts/changemail.py
+++ scripts/changemail.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2005-2008 rPath, Inc.
+# Copyright (C) 2005-2009 rPath, Inc.
 #
 # This program is distributed under the terms of the Common Public License,
 # version 1.0. A copy of this license should have been distributed with this
@@ -55,6 +55,7 @@ def process(repos, cfg, commitList, srcMap, pkgMap, grpMap, argv, otherArgs):
         'binaryuser': options.ONE_PARAM,
         'from': options.ONE_PARAM,
         'email': options.MULT_PARAM,
+        'maxsize': options.ONE_PARAM,
     }
 
     # create an argv[0] for processArgs to ignore
@@ -77,6 +78,7 @@ def process(repos, cfg, commitList, srcMap, pkgMap, grpMap, argv, otherArgs):
     sourceuser = None
     binaryuser = None
     fromaddr = None
+    maxsize = None
     if 'sourceuser' in argSet:
         sourceuser = argSet['sourceuser']
     if 'binaryuser' in argSet:
@@ -87,6 +89,8 @@ def process(repos, cfg, commitList, srcMap, pkgMap, grpMap, argv, otherArgs):
         binaryuser = argSet['user']
     if 'from' in argSet:
         fromaddr = argSet['from']
+    if 'maxsize' in argSet:
+        maxsize = int(argSet['maxsize'])
 
     pid = os.fork()
     if not pid:
@@ -94,7 +98,7 @@ def process(repos, cfg, commitList, srcMap, pkgMap, grpMap, argv, otherArgs):
         pid2 = os.fork()
         if not pid2:
             #child 2
-            doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr, argSet)
+            doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr, maxsize, argSet)
             sys.exit(0)
         else:
             #parent 2
@@ -105,7 +109,7 @@ def process(repos, cfg, commitList, srcMap, pkgMap, grpMap, argv, otherArgs):
     return 0
 
 
-def doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr, argSet):
+def doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr, maxsize, argSet):
     tmpfd, tmppath = tempfile.mkstemp('', 'changemail-')
     os.unlink(tmppath)
     tmpfile = os.fdopen(tmpfd)
@@ -142,7 +146,7 @@ def doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr,
         if sourceuser:
             print 'Committed by: %s' %sourceuser
 
-        sendMail(tmpfile, subject, fromaddr, argSet['email'])
+        sendMail(tmpfile, subject, fromaddr, maxsize, argSet['email'])
 
     if pkgMap or grpMap:
         # stdout is the tmpfile
@@ -186,18 +190,27 @@ def doWork(repos, cfg, srcMap, pkgMap, grpMap, sourceuser, binaryuser, fromaddr,
                         ', '.join(flavor.split(',')))
                 print
 
-        sendMail(tmpfile, subject, fromaddr, argSet['email'])
+        sendMail(tmpfile, subject, fromaddr, maxsize, argSet['email'])
         os.dup2(oldStdOut, 1)
 
     return 0
 
-def sendMail(tmpfile, subject, fromaddr, addresses):
+def sendMail(tmpfile, subject, fromaddr, maxsize, addresses):
     # stdout is the tmpfile, so make sure it has been flushed!
     sys.stdout.flush()
     if fromaddr:
         fromarg = "-r '%s'" %fromaddr
     else:
         fromarg = ''
+
+    if maxsize:
+        tmpfile.seek(0, 2)
+        size = tmpfile.tell()
+        if size > maxsize:
+            tmpfile.truncate(maxsize-6)
+            tmpfile.seek(0, 2)
+            tmpfile.write('\n...\n')
+
     for address in addresses:
         tmpfile.seek(0)
         mail = util.popen("""mail -s '%s' %s '%s'""" %(subject, fromarg, address), "w")
