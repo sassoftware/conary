7418320be393fd5536b4e3ef10a3c270dc63a37c
Erik Troan <http://issues.rpath.com>
2009-10-21 11:35:10 -0400

    update copyright dates

--- conary/build/defaultrecipes.py
+++ conary/build/defaultrecipes.py
@@ -1,3 +1,17 @@
+#
+# Copyright (c) 2009 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
 BaseRequiresRecipe = '''
 class BaseRequiresRecipe(Recipe):
     """
