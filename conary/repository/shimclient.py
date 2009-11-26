8bb783ecfaa311e0e29f737e82fd3d39864a8e61
Michael K. Johnson <http://issues.rpath.com/>
2009-11-25 20:01:36 -0500

    revert unintended change in commitChangeSet call (CNY-3295)

--- conary/repository/shimclient.py
+++ conary/repository/shimclient.py
@@ -186,7 +186,7 @@ class ShimNetClient(netclient.NetworkRepositoryClient):
         url = util.normurl(self.c[newLabel]._server.basicUrl) + "?" + base
 
         self.c[newLabel].commitChangeSet(url, mirror = mirror,
-                                         hidden = hidden, callback=callback)
+                                         hidden = hidden)
 
     def commitChangeSetFile(self, fName, mirror = False, callback = None,
                             hidden = False):
