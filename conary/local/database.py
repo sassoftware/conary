2f1056978718ba766f545a2297b9e85a79a6c417
Whitney Battestilli <http://issues.rpath.com/>
2010-02-08 15:32:00 -0500

    imported patch CNY-3327

--- conary/local/database.py
+++ conary/local/database.py
@@ -2145,7 +2145,8 @@ class Database(SqlDbRepository):
             fsJob.runPostScripts(tagScript)
 
     def runPreScripts(self, uJob, callback, tagScript = None,
-                      justDatabase = False, tmpDir = '/', jobIdx = None):
+                      justDatabase = False, tmpDir = '/', jobIdx = None,
+                      abortOnError = False):
         if justDatabase:
            return True
 
@@ -2165,9 +2166,15 @@ class Database(SqlDbRepository):
                                        scriptId = scriptId,
                                        oldCompatClass = oldCompatClass,
                                        newCompatClass = newCompatClass)
+
             uJob.recordPreScriptRun(action, job)
+
             if rc:
-                return False
+                if abortOnError:
+                    raise PreScriptError(script, rc, "Script returned with a "
+                                         "non-zero exist status." )
+                else:
+                    return False
 
         return True
 
@@ -2448,7 +2455,7 @@ class Database(SqlDbRepository):
     def _applyRollbackList(self, repos, names, replaceFiles = False,
                           callback = UpdateCallback(), tagScript = None,
                           justDatabase = False, transactionCounter = None,
-                          lazyCache = None):
+                          lazyCache = None, abortOnError = False):
         assert transactionCounter is not None, ("The transactionCounter "
             "argument is mandatory")
         if transactionCounter != self.getTransactionCounter():
@@ -2537,7 +2544,8 @@ class Database(SqlDbRepository):
 
                     self.runPreScripts(updJob, callback = callback,
                                        tagScript = tagScript,
-                                       justDatabase = justDatabase)
+                                       justDatabase = justDatabase,
+                                       abortOnError = abortOnError)
 
                     fsUpdateJob = UpdateJob(None, lazyCache = lazyCache)
                     if not reposCs.isEmpty():
@@ -2864,6 +2872,24 @@ class RollbackError(errors.ConaryError):
 
     def __str__(self):
 	return "rollback %s cannot be applied:\n%s" % (self.name, self.error)
+
+class PreScriptError(errors.ConaryError):
+
+    """Raised for exceptions related to executing pre-scripts"""
+
+    def __init__(self, script, errorCode, errorMessage=''):
+	"""
+	@param script: string represeting the path of the failed script
+        @param errorCode: the return code of the script
+        """
+	self.name = script
+        self.errorCode = errorCode
+        self.error = errorMessage
+
+    def __str__(self):
+	return "Pre-Script\n\"%s\"\nhas failed. Return Code=%s\n%s" % (self.name,
+                                                         self.errorCode,
+                                                         self.error)
 
 class RollbackOrderError(RollbackError):
 
