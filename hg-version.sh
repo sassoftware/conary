22bc6fb3508d4b77a44456c055315736e25bde64
Whitney Battestilli <http://issues.rpath.com/>
2010-03-16 21:24:21 -0400

    added a checksum variable to constants.py

---
+++ hg-version.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Copyright (c) 2009 rPath, Inc.
+# All rights reserved
+#
+# Get the build version for build.xml
+
+hgDir=.
+if [[ -x /usr/bin/hg && -d $hgDir/.hg ]] ; then
+    rev=`hg id -i`
+elif [ -f $hgDir/.hg_archival.txt ]; then
+    rev=`grep node $hgDir/.hg_archival.txt |cut -d' ' -f 2 |head -c 12`;
+else
+    rev= ;
+fi ;
+echo "$rev"
+
