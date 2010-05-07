0508cdb3e0aef0a4752802439cf7fbe5bec63f88
Whitney Battestilli <http://issues.rpath.com/>
2010-05-07 00:15:00 -0400

    fix for compatability with rbuild (RBLD-313)

--- conary/build/derive.py
+++ conary/build/derive.py
@@ -18,29 +18,24 @@ resulting packages to the repository.
 """
 
 import os
-import stat
 import shutil
-import re
 
-from conary import branch
 from conary import checkin
 from conary import conaryclient
 from conary import errors
 from conary import state
 from conary import updatecmd
-from conary import versions
-from conary.deps import deps
 from conary.lib import log, util
-
+from conary.versions import Label
 class DeriveCallback(checkin.CheckinCallback):
     def setUpdateJob(self, *args, **kw):
         # stifle update announcement for extract
         pass
 
-def derive(repos, cfg, targetLabel, troveSpec, checkoutDir = None,
+def derive(repos, cfg, targetLabel, troveToDerive, checkoutDir = None,
            extract = False, info = False, callback = None):
     """
-        Creates a derived recipe. Note that is does not actually commit 
+        Creates a derived recipe. Note that is does not actually commit
         anything to the repository.
 
         Finally if extract = True, it installs an version of the binary
@@ -51,6 +46,8 @@ def derive(repos, cfg, targetLabel, troveSpec, checkoutDir = None,
         @type cfg: ConaryConfiguration object
         @param targetLabel: label to derive from
         @type targetLabel: versions.Label
+        @param troveToDerive the trove to derive from
+        @type (n,v,f) or a troveSpec
         @param checkoutDir: directory to create the checkout in.  If None,
                              defaults to currentDir + packageName.
         @param extract: If True, creates a subdirectory of the checkout named
@@ -61,16 +58,28 @@ def derive(repos, cfg, targetLabel, troveSpec, checkoutDir = None,
 
     if callback is None:
         callback = DeriveCallback()
-    troveName, versionSpec, flavor = conaryclient.cmdline.parseTroveSpec(
-                                                                    troveSpec)
+    if isinstance(troveToDerive,tuple):
+        troveName, versionSpec, flavor = troveToDerive
+        versionSpec = str(versionSpec)
+        #flavor = str(flavor)
+        troveSpec = conaryclient.cmdline.toTroveSpec(troveName,
+                                                     versionSpec,
+                                                     flavor)
+    else:
+        troveSpec = troveToDerive
+        troveName, versionSpec, flavor = conaryclient.cmdline.parseTroveSpec(
+            troveSpec)
+
+    if isinstance(targetLabel,str):
+        targetLabel = Label(targetLabel)
+
+    nvfToDerive, = repos.findTrove(cfg.buildLabel, (troveName, versionSpec,
+                                                        flavor), cfg.flavor)
+    troveToDerive = repos.getTrove(*nvfToDerive)
 
     if ":" in troveName:
         raise errors.ParseError('Cannot derive individual components: %s' %
                                 troveName)
-
-    nvfToDerive, = repos.findTrove(cfg.buildLabel, (troveName, versionSpec,
-        flavor), cfg.flavor)
-    troveToDerive = repos.getTrove(*nvfToDerive)
 
     client = conaryclient.ConaryClient(cfg,repos=repos)
     laterShadows = client._checkForLaterShadows(targetLabel, [troveToDerive])
@@ -81,7 +90,7 @@ def derive(repos, cfg, targetLabel, troveSpec, checkoutDir = None,
                        'derive from %s=%s[%s] but %s=%s[%s] is already '
                        'shadowed on this label.'
                        % (n, shadowedVer, f, n, v, f))
-            raise BranchError('\n\n'.join(msg))
+            raise errors.BranchError('\n\n'.join(msg))
 
     # displaying output along the screen allows there to be a record
     # of what operations were performed.  Since this command is
@@ -99,11 +108,18 @@ def derive(repos, cfg, targetLabel, troveSpec, checkoutDir = None,
     nvfs = list(troveToDerive.iterTroveList(strongRefs=True))
     trvs = repos.getTroves(nvfs,withFiles=False)
     hasCapsule = [ x for x in trvs if x.troveInfo.capsule.type() ]
+    removeText = \
+"""
+        # This appliance uses PHP as a command interpreter but does
+        # not include a web server, so remove the file that creates
+        # a dependency on the web server
+        r.Remove('/etc/httpd/conf.d/php.conf')
+"""
     if hasCapsule:
         derivedRecipeType = 'DerivedCapsuleRecipe'
+        removeText = ''
     else:
         derivedRecipeType = 'DerivedPackageRecipe'
-
     shadowBranch = shadowedVersion.branch()
 
     checkoutDir = checkoutDir or troveName
@@ -121,12 +137,30 @@ class %(className)sRecipe(%(recipeBaseClass)s):
     version = '%(version)s'
 
     def setup(r):
-        pass
+        '''
+        In this recipe, you can make modifications to the package.
 
+        Examples:
+
+        # This appliance has high-memory-use PHP scripts
+        r.Replace('memory_limit = 8M', 'memory_limit = 32M', '/etc/php.ini')
+%(removeText)s
+        # This appliance requires that a few binaries be replaced
+        # with binaries built from a custom archive that includes
+        # a Makefile that honors the DESTDIR variable for its
+        # install target.
+        r.addArchive('foo.tar.gz')
+        r.Make()
+        r.MakeInstall()
+
+        # This appliance requires an extra configuration file
+        r.Create('/etc/myconfigfile', contents='some data')
+        '''
 """ % dict(className=className,
            name=troveName,
            version=shadowedVersion.trailingRevision().getVersion(),
-           recipeBaseClass=derivedRecipeType)
+           recipeBaseClass=derivedRecipeType,
+           removeText=removeText)
     open(os.sep.join((checkoutDir,recipeName)), 'w').write(derivedRecipe)
 
     oldBldLabel = cfg.buildLabel
