5dd86087f045bd996701b23f673572d037387b5f
Erik Troan <http://issues.rpath.com>
2010-07-30 10:48:20 -0400

    avoid referenceing the content store in get method unless it's needed

--- conary/server/server.py
+++ conary/server/server.py
@@ -92,11 +92,11 @@ class HttpRequests(SimpleHTTPRequestHandler):
         return path
 
     def do_GET(self):
-        def _writeNestedFile(outF, name, tag, size, f, contentStore, sizeCb):
+        def _writeNestedFile(outF, name, tag, size, f, netRepos, sizeCb):
             if changeset.ChangedFileTypes.refr[4:] == tag[2:]:
                 sha1, size = f.read().split(' ')
                 size = int(size)
-                path = contentStore.hashToPath(sha1)
+                path = netRepos.repos.repos.contentsStore.hashToPath(sha1)
                 f = open(path)
                 tag = tag[0:2] + changeset.ChangedFileTypes.file[4:]
 
@@ -167,8 +167,7 @@ class HttpRequests(SimpleHTTPRequestHandler):
                     cs.dump(self.wfile.write,
                             lambda name, tag, size, f, sizeCb:
                                 _writeNestedFile(self.wfile, name, tag, size, f,
-                                                 self.netRepos.repos.repos.contentsStore,
-                                                 sizeCb))
+                                                 self.netRepos, sizeCb))
 
                     del cs
                 else:
