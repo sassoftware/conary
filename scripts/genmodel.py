ac5a1486ed7b9423d6bb22c0141ea666c7f02512
Whitney Battestilli <http://issues.rpath.com/>
2010-11-04 17:19:04 -0400

    pyflakes fix

--- scripts/genmodel.py
+++ scripts/genmodel.py
@@ -35,8 +35,8 @@ from conary.cmds import updatecmd
 from conary.deps import deps
 from conary.trovetup import TroveSpec
 
-# pyflakes=ignore
-from conary.lib import log
+from conary.lib import log #  pyflakes=ignore
+
 
 OrigFindAction = modelupdate.CMLFindAction
 class TrackFindAction(OrigFindAction):
