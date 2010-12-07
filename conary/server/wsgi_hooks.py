11cc1afd7aa2ede8040b50b254c36935483f4e4d
Michael Tharp <mtharp@rpath.com>
2010-12-07 16:17:54 -0500

    Fix XMLRPC hangs when using uWSGI

--- conary/server/wsgi_hooks.py
+++ conary/server/wsgi_hooks.py
@@ -14,6 +14,7 @@
 import errno
 import logging
 import os
+import traceback
 import xmlrpclib
 from email.Message import Message
 
@@ -31,7 +32,23 @@ log = logging.getLogger('wsgi_hooks')
 _config_cache = {}
 
 
-class application(object):
+def application(environ, start_response):
+    """Wrapper application that catches high-level errors."""
+    server = WSGIServer(environ, start_response)
+    yielded = False
+    try:
+        for chunk in server:
+            yield chunk
+            yielded = True
+    except:
+        traceback.print_exc()
+        if not yielded:
+            headers = [('Content-type', 'text/plain')]
+            start_response('500 Internal Server Error', headers)
+            yield "ERROR: Internal server error.\r\n"
+
+
+class WSGIServer(object):
 
     def __init__(self, environ, start_response):
         self.environ = environ
@@ -277,7 +294,10 @@ class application(object):
             return
 
         # TODO: pipeline
-        stream = self.environ['wsgi.input']
+        try:
+            stream = self._getStream()
+        except WrappedResponse, wrapper:
+            yield wrapper.response
         encoding = self.environ.get('HTTP_CONTENT_ENCODING')
         if encoding == 'deflate':
             stream = util.decompressStream(stream)
@@ -478,6 +498,19 @@ class application(object):
                 return open(path, 'wb+')
         return None
 
+    def _getStream(self):
+        if self.environ.get('HTTP_TRANSFER_ENCODING', 'identity') != 'identity':
+            raise WrappedResponse(self._response('400 Bad Request',
+                    "ERROR: Unrecognized Transfer-Encoding\r\n"))
+        try:
+            size = int(self.environ['CONTENT_LENGTH'])
+        except ValueError:
+            raise WrappedResponse(self._response('411 Length Required',
+                    "ERROR: Invalid or missing Content-Length\r\n"))
+
+        stream = self.environ['wsgi.input']
+        return LengthConstrainingWrapper(stream, size)
+
     def close(self):
         log.info("... closing")
         # Make sure any pooler database connections are released.
@@ -485,5 +518,30 @@ class application(object):
             self.repositoryServer.reset()
 
 
+class LengthConstrainingWrapper(object):
+    """
+    A file-like object that returns at most C{size} bytes before pretending to
+    hit end-of-file.
+    """
+
+    def __init__(self, fobj, size):
+        self.fobj = fobj
+        self.remaining = size
+
+    def read(self, size=None):
+        if size is None:
+            size = self.remaining
+        else:
+            size = min(size, self.remaining)
+        self.remaining -= size
+        return self.fobj.read(size)
+
+
 class ConfigurationError(RuntimeError):
     pass
+
+
+class WrappedResponse(Exception):
+
+    def __init__(self, response):
+        self.response = response
