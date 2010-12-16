97a53ac38b496ef96aab78c8c417fe49f8311b1f
Mihai Ibanescu <https://issues.rpath.com/>
2010-12-16 17:17:30 -0500

    Resetting isEncapsulatedContent for regular capsule policy (CNY-3577)

--- conary/build/capsulepolicy.py
+++ conary/build/capsulepolicy.py
@@ -570,3 +570,5 @@ class CapsuleModifications(policy.Policy):
                     f.flags.isCapsuleOverride(True)
                     if not capPath:
                         f.flags.isCapsuleAddition(True)
+                    # CNY-3577
+                    f.flags.isEncapsulatedContent(False)
