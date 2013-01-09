cf94b7055619b971b080759b9394d14b3a2cd061
Michael Tharp <michael.tharp@sas.com>
2013-01-09 19:13:57 +0000

    Regenerate cython and fix mangled C file

--- conary/lib/ext/helper_sha256_nonstandard.c
+++ conary/lib/ext/helper_sha256_nonstandard.c
@@ -1,20 +1,4 @@
 /*
- * Copyright (c) SAS Institute Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
  NOTE: DO NOT USE unless you need an implementation of sha256 which has
  an implementation error when the length of the string being
  hashed is 55 % 64.  See http://pycrypto.cvs.sourceforge.net/viewvc/pycrypto/crypto/src/SHA256.c?r1=1.3&r2=1.4 for fix to this bug.  In this case,
