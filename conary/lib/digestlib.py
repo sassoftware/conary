7a1cc825ba0c79726bcf141bfe277c6b19c7276a
Michael Tharp <michael.tharp@sas.com>
2013-05-23 15:04:41 +0000

    Require Python 2.6 or later

--- conary/lib/digestlib.py
+++ conary/lib/digestlib.py
@@ -13,37 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# pyflakes=ignore-file
 
+"""
+Formerly backwards compatibility for Python 2.4, now Python 2.6 is required so
+this is just here to avoid breaking imports from other projects.
+"""
 
-"Compatibility module for python 2.4 - 2.6"
+from hashlib import sha1, md5, sha224, sha256, sha384, sha512
 
-try:
-    import hashlib
-    sha1 = hashlib.sha1
-    md5 = hashlib.md5
-    sha224 = hashlib.sha224
-    sha256 = hashlib.sha256
-    sha384 = hashlib.sha384
-    sha512 = hashlib.sha512
-except ImportError:
-    import sha
-    import md5
-    from Crypto.Hash import SHA256
-    sha1 = sha.new
-    md5 = md5.new
-    sha256 = SHA256.new
-    sha224 = sha384 = sha512 = None
-
-
-
-
-# Bug check - a version of sha256 we used to distribute miscalculated sha256
-# for strings of length 55 (mod 64).  If we're using such a version, then
-# we don't have a standard sha256 implementation.
-isStandardSha256 = sha256('\0' * 55).hexdigest() == '02779466cdec163811d078815c633f21901413081449002f24aa3e80f0b88ef7'
-if not isStandardSha256:
-    sha256 = None
-del isStandardSha256
-
-# import backwards compatible version of sha256 with this calculation bug.
+# import backwards compatible version of sha256 with calculation bug.
 from conary.lib.ext.sha256_nonstandard import digest as sha256_nonstandard
