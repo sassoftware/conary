280a51e6d3352cc7356611a7330fe004c92f1a98
Michael Tharp <michael.tharp@sas.com>
2014-03-19 18:39:31 +0000

    Fix crash if useDirs is empty but archDirs is not

--- conary/flavorcfg.py
+++ conary/flavorcfg.py
@@ -201,7 +201,7 @@ class FlavorConfig:
                         self.flags[flag] = UseFlagConfig(flag)
                     self.flags[flag].read(flagPath)
         for archDir in archDirs:
-            useDir = os.path.expanduser(useDir)
+            archDir = os.path.expanduser(archDir)
             if archDir and os.path.exists(archDir):
                 for arch in os.listdir(archDir):
                     if (os.path.isfile(os.path.join(archDir, arch)) and
